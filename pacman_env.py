6fe10020b56edce029ee818c0a17f21d11766539
istvan-knab <97619115+istvan-knab@users.noreply.github.com>
2022-09-17 21:07:05 +0200

    def __init__ + main

--- pacman_env.py
+++ pacman_env.py
@@ -2,17 +2,54 @@
 # Author : Csorba Levente , Knáb István Gellért
 #Team : I_NEED_A
 
+
+#import the needed libraries
+import cv2
+import numpy as np
+
 class PacMan:
-    def __init__(self):
-        pass
+    def __init__(self, map_size):
+        self.map_size = map_size
+        # lists for the map components
+        self.body = []
+        self.objects = []
+        # initiate default values
+        self.step_ = 0
+        self.dir = 1
+        self.size = 5
+        self.last_obs = None
+        # Variables for visualizing the current grid world
+        self.show_img_size = 300
+        self.show_img = np.zeros((self.show_img_size, self.show_img_size, 3))
+        self.ratio = int(self.show_img_size / self.map_size)
+        self.reset()
 
 
     def step(self):
         pass
 
-    
+
     def reset(self):
         pass
 
+
+    def render(self):
+        pass
+
+
+def initialize_parameters():
+    global map_size , done
+    map_size = 20
+    done_ = False
+
+
 if __name__ == "__main__":
-    pass
+
+    #initial parameters
+    initialize_parameters()
+
+    #Calling Constructor for the game
+    env = PacMan(map_size=map_size)
+
+    #starting state
+    state = env.reset()
