c24a4c3b60df6aebf85e68df197fcf3aeacd19dd
BPelenczei <81440132+BPelenczei@users.noreply.github.com>
2022-09-17 11:51:05 +0200

    ADD reset function definition for the Pacman environment class (not implemented yet)

--- basic_pacman/pacman_env.py
+++ basic_pacman/pacman_env.py
@@ -13,4 +13,12 @@ class Pacman:
         self.step = 0
         self.score = 0
         self.orientation = 0
-        self.state = None
+
+        self.state = self.reset()
+
+    def reset(self):
+        """
+        This function resets the game session and returns a newly generated game state.
+        :return: current state of the game (empty map with the pacman, ghosts and coins spawned)
+        """
+        raise NotImplementedError
