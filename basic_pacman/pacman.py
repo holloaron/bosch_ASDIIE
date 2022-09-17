f2192c67cce6fcbd3b3f67179635c08a6c7205bb
BPelenczei <81440132+BPelenczei@users.noreply.github.com>
2022-09-17 12:03:09 +0200

    ADD main function to core file (game loop)

--- basic_pacman/pacman.py
+++ basic_pacman/pacman.py
@@ -1,1 +1,10 @@
 from basic_pacman.pacman_env import Pacman
+
+if __name__ == "__main__":
+    env = Pacman()
+    done = False
+    state = env.reset()
+    while not done:
+        action = int(input("Choose your action:\n"))
+        state, reward, done, info = env.step(action=action)
+        env.render()
