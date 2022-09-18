0bb2afcbda593fbeaf3b54527be4e16998b42616
farkas-peter <peterfarkas@edu.bme.hu>
2022-09-18 20:11:45 +0200

    ADD: empty map w/o walls, game terminates after max time steps

---
+++ Namco/pacman.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+
+class PacMan:
+    def __init__(self, map_size=10, max_time_step=100):
+        # PacMan variables
+        self.map_size = map_size
+        self.time_step = 0
+        self.max_time_step = max_time_step
+        self.map = np.zeros((map_size, map_size))
+        self.x = np.random.randint(0, self.map_size)
+        self.y = np.random.randint(0, self.map_size)
+
+    def reset(self):
+        pass
+
+    def step(self):
+        done = False
+
+        # Increasing the timestep
+        self.time_step += 1
+
+        # Terminating after max time steps
+        if self.time_step > self.max_time_step:
+            done = True
+
+        return done
+
+    def render(self):
+        pass
+
+
+if __name__ == "__main__":
+    # Instantiating the environment
+    env = PacMan(map_size=10,
+                 max_time_step=100)
+
+    done_ = False
+    while not done_:
+        done_ = env.step()
+
+
