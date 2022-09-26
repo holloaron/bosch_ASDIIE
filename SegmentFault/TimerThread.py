e28aef32bcf6edec0bdfda2629e8f669391a7bd9
markbozsoki <83731022+markbozsoki@users.noreply.github.com>
2022-09-27 01:43:23 +0200

    CHD: TimerThread.py - changed commenting style to protocol

--- SegmentFault/TimerThread.py
+++ SegmentFault/TimerThread.py
@@ -15,36 +15,23 @@
 /*********************************************************************
 /*********************************************************************
 """
-
-"""
-//*********************************************************************
-//*********************************************************************
-                            INCLUDES                              
-//*********************************************************************
-//*********************************************************************
-"""
-
 import time
 import threading
 
-"""
-
-    TimeCounter class
-    inherits from python thread class
-    @params threading.Thread
-
-"""
-
-
 class TimeCounter(threading.Thread):
+    """ inherits from python thread class
+    
+    @args:
+        threading.Thread
     """
-        Timer Init
-        Inits the timer variables
-        @params self, *args **kwargs
-
-    """
-
     def __init__(self, *args, **kwargs):
+        """ Inits the timer variables
+        
+        @args:
+            self,
+            *args [] - 
+            **kwargs [] -
+        """
         super(TimeCounter, self).__init__(*args, **kwargs)
         self._flag = threading.Event()  # flag to pause the thread
         self._flag.set()  # Set true
@@ -52,64 +39,56 @@ class TimeCounter(threading.Thread):
         self._running.set()  # set running to True
         self._seconds_passed = 0  # value to track passed seconds
 
-    """
-
-        run method
-        Starts the Timer Thread, increase the value of _seconds_passed by 1 in each iteration (-> 1s passed)
-        @params self
-
-    """
 
     def run(self):
+        """ Starts the Timer Thread, increase the value of _seconds_passed by 1 in each iteration (-> 1s passed)
+        
+        @args:
+            self
+        """
         while self._running.isSet():
             self._flag.wait()
             self._seconds_passed = self._seconds_passed + 1
             time.sleep(1)
 
-    """
-
-        pause method
-        Pauses the timer by blocking the Thread 
-        @params self
-
-    """
 
     def pause(self):
+        """ Pauses the timer by blocking the Thread
+        
+        @args:
+            self
+        """
         self._flag.clear()  # Set the flag false -> block the thread
 
-    """
-
-        resume method
-        Resumes the timer by stop blocking  the Thread 
-        @params self
-
-    """
 
     def resume(self):
+        """ Resumes the timer by stop blocking  the Thread
+        
+        @args:
+            self
+        """
         self._flag.set()  # stop blocking of the thread
 
-    """
-
-        stop method
-        Stops the timer 
-        @params self
-
-    """
 
     def stop(self):
+        """ Stops the timer
+
+        @args:
+            self
+        """
+
         self._flag.set()  # Resume the thread from the suspended state, it is already suspended
         self._running.clear()  # Set false -> block the thread
 
-    """
-
-        reset method
-        Resets the _seconds_passed variable
-        @params self
-
-    """
 
     def reset(self):
+        """ Resets the _seconds_passed variable
+
+        @args:
+            self
+        """
         self._seconds_passed = 0
+
 
     @property
     def seconds_passed(self):
