2de32c755a30ab1670375345559fb834467e1b7a
Czuri Dávid <david.czuri@consteelsoftware.com>
2022-09-27 22:46:16 +0200

    ADD game window opens on program start

--- TeamSix/pacman.py
+++ TeamSix/pacman.py
@@ -1,1 +1,16 @@
- comment
+import pygame
+
+WINDOW_HEIGHT = 900
+WINDOW_WIDTH = 900
+
+
+class Pacman:
+
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption('TeamSix Pacman')
+        self.game_window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+
+if __name__ == '__main__':
+    game = Pacman()
