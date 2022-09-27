c16ed1e069c0cbbe7da54bcdee0c3f493f2c1f5c
4doschris <fordos.krisztian@gmail.com>
2022-09-27 13:34:51 +0200

    Add Initialize function
    Initialize function has been added to the code. It defines the default values.

--- pacman.py
+++ pacman.py
@@ -1,3 +1,25 @@
 """""
 created by Git-egylet
 """""
+
+import cv2
+import numpy as np
+
+class Pacman:
+    def __init__(self, world_size):
+        self.world_size = world_size
+        # map components list
+        self.body = []
+        self.wall = []
+        self.ghost = []
+        # default values
+        self.step = 0
+        self.direction = 1
+        self.score = 0
+        self.last_state = None
+        # variables for world
+        self.show_window_size = 300
+        self.show_window_size = np.zeros((self.show_window_size, self.show_window_size, 3))
+        self.ratio = int(self.show_window_size / self.world_size)
+        self.reset()
+        
