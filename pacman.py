89fb79154f2d82e74d65350a43b2d18dcd239c6e
gujgidani <gujgiczerd@edu.bme.hu>
2022-10-01 09:14:01 +0200

    Add Docstring and Typehint to left function

--- pacman.py
+++ pacman.py
@@ -156,7 +156,13 @@ class Pacman:
         pos_y -= 1
         return pos_x, pos_y
 
-    def _left(self, pos_x, pos_y):
+    def _left(self, pos_x: int, pos_y: int):
+        """
+        This function moves Pacman left
+        :param pos_x: horizontal coordinate
+        :param pos_y: vertical coordinate
+        :return: pos_x, pos_y (int): the updated coordinates
+        """
         pos_x -= 1
         return pos_x, pos_y
 
