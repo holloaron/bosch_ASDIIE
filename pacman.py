00ffbc865de9ef1f14498b3a0c8df527b4147ee4
gujgidani <gujgiczerd@edu.bme.hu>
2022-10-01 22:14:09 +0200

    Fix bug: left function decreases wrong coordinate
    For more information about the bug, check the description of commit 9501134

--- pacman.py
+++ pacman.py
@@ -165,7 +165,7 @@ class Pacman:
         :param pos_y: vertical coordinate
         :return: pos_x, pos_y (int): the updated coordinates
         """
-        pos_x -= 1
+        pos_y -= 1
         return pos_x, pos_y
 
     def _check_pellets(self, x: int, y: int, score: int):
