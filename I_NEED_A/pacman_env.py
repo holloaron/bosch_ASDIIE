d2a4fcb693a487d022eebb6d7217bafae615780e
istvan-knab <97619115+istvan-knab@users.noreply.github.com>
2022-10-01 13:28:23 +0200

    remove unused lines

--- I_NEED_A/pacman_env.py
+++ I_NEED_A/pacman_env.py
@@ -95,7 +95,8 @@ class PacMan:
 
         else:
             #in case of wrong key
-            raise TypeError("Only integers brtween 0 and 3 are accepted")
+            if action != 0 or action != 1 or action != 2 or action != 3:
+                raise TypeError("Only integers brtween 0 and 3 are accepted")
             done_ = True
             self.score= -1
             print(f"Score {self.score}")
