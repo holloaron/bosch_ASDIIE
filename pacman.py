846216965ceae3b8e6fdf21f089ed3397b2818db
Czeilinger96 <cecebalu@gmail.com>
2022-10-15 17:40:24 +0200

    Creat dot class

--- pacman.py
+++ pacman.py
@@ -15,11 +15,22 @@ YELLOW = (255, 255, 0)
 
 # Screen, character and game settings
 SCREEN_SIZE = 1000
+DOT_SIZE = (20, 20)
 
 #Game window creation and title
 DISPLAYSURF = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
 DISPLAYSURF.fill(BLACK)
 pygame.display.set_caption("PACMAN")
+
+class DOT(pygame.sprite.Sprite):
+    def __init__(self):
+
+        #Dot load from file and transform to the required size
+
+        super().__init__()
+        self.image = pygame.image.load('dot.png')
+        self.image = pygame.transform.scale(self.image, DOT_SIZE)
+        self.rect = self.image.get_rect()
 
 if __name__ == "__main__":
 
