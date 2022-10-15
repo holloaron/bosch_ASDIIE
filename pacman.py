560bbea6f8e5089c3051eb5c16df672dd19ed9d3
Czeilinger96 <cecebalu@gmail.com>
2022-10-15 17:44:30 +0200

    Creat player class. Create the function to move the player.

--- pacman.py
+++ pacman.py
@@ -15,6 +15,10 @@ YELLOW = (255, 255, 0)
 
 # Screen, character and game settings
 SCREEN_SIZE = 1000
+PACMAN_SIZE = (50, 50)
+PACMAN_STARTING_POS_X = 160
+PACMAN_STARTING_POS_Y = 520
+PACMAN_SPEED = 5
 DOT_SIZE = (20, 20)
 
 #Game window creation and title
@@ -31,6 +35,40 @@ class DOT(pygame.sprite.Sprite):
         self.image = pygame.image.load('dot.png')
         self.image = pygame.transform.scale(self.image, DOT_SIZE)
         self.rect = self.image.get_rect()
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+
+        # Character load from file, transform to the required size and display in the given position
+
+        super().__init__()
+        self.image = pygame.image.load("Pacman.png")
+        self.image = pygame.transform.scale(self.image, PACMAN_SIZE)
+        self.rect = self.image.get_rect()
+        self.rect.center = (PACMAN_STARTING_POS_X, PACMAN_STARTING_POS_Y)
+
+    def move(self):
+
+        # This function moves the character accordingly to the arrow key you press and check if we go through the wall
+
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -PACMAN_SPEED)
+            if self.rect.top < 0:
+                self.rect.move_ip(0, SCREEN_SIZE)
+        elif pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, PACMAN_SPEED)
+            if self.rect.bottom > SCREEN_SIZE:
+                self.rect.move_ip(0, -SCREEN_SIZE)
+        elif pressed_keys[K_LEFT]:
+            self.rect.move_ip(-PACMAN_SPEED, 0)
+            if self.rect.left < 0:
+                self.rect.move_ip(SCREEN_SIZE, 0)
+        elif pressed_keys[K_RIGHT]:
+            self.rect.move_ip(PACMAN_SPEED, 0)
+            if self.rect.right > SCREEN_SIZE:
+                self.rect.move_ip(-SCREEN_SIZE, 0)
 
 if __name__ == "__main__":
 
