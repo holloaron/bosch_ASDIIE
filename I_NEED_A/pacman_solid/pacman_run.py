cfe3057f8ce3e3472fb81ec758862bf7cc5468ec
isti_knab <istvan.knab@edu.bme.hu>
2022-10-24 00:23:45 +0200

    ADD FILE: initialize new project and declare the tasks

---
+++ I_NEED_A/pacman_solid/pacman_run.py
@@ -0,0 +1,12 @@
+#TODO:import other classes
+#TODO:import packages
+if __name__ == "__main__":
+
+    while(1):
+
+        #TODO:represent choosing an action
+
+        #TODO:represent step
+
+        #TODO:Realize render
+        break
