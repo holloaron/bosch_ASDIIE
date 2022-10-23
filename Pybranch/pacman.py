b804422adc45225dcaa973630983fd4e9b5c91a7
Czeilinger96 <cecebalu@gmail.com>
2022-10-23 17:43:05 +0200

    Added a new function, stop after a given time. Fixing the inconsistent names of the variables.

--- Pybranch/pacman.py
+++ Pybranch/pacman.py
@@ -22,36 +22,45 @@ PACMAN_SPEED = 5
 DOT_SIZE = (20, 20)
 DOT_NO_SPAWN_FRAME = 40
 MAX_SCORE = 60
+MAX_TIME = 45000
 SCORE = 0
 SCORE_POS = 10
+TIME_POS = 950
 
-#Font parameters
+# Font parameters
 FONT_SIZE = 60
 FONT_SMALL_SIZE = 20
 font = pygame.font.SysFont("Verdana", FONT_SIZE)
 font_small = pygame.font.SysFont("Verdana", FONT_SMALL_SIZE)
 game_over = font.render("Game Over", True, YELLOW)
-game_over_pos_x = 320
-game_over_pos_y = 500
+GAME_OVER_POS_X = 320
+GAME_OVER_POS_Y = 500
 
-#Game window creation and title
+# Game window creation and title
 DISPLAYSURF = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
 DISPLAYSURF.fill(BLACK)
 pygame.display.set_caption("PACMAN")
 
-class DOT(pygame.sprite.Sprite):
-    def __init__(self):
 
-        #Dot load from file and transform to the required size
+class Dot(pygame.sprite.Sprite):
+    def __init__(self, frame_min, frame_max):
+        self.frame_min = frame_min
+        self.frame_max = frame_max
+
+        # Dot load from file and transform to the required size
 
         super().__init__()
         self.image = pygame.image.load('dot.png')
         self.image = pygame.transform.scale(self.image, DOT_SIZE)
         self.rect = self.image.get_rect()
+        self.rect.x = random.randint(frame_min, frame_max)
+        self.rect.y = random.randint(frame_min, frame_max)
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, pos_x, pos_y):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
 
         # Character load from file, transform to the required size and display in the given position
 
@@ -59,7 +68,7 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load("Pacman.png")
         self.image = pygame.transform.scale(self.image, PACMAN_SIZE)
         self.rect = self.image.get_rect()
-        self.rect.center = (PACMAN_STARTING_POS_X, PACMAN_STARTING_POS_Y)
+        self.rect.center = (self.pos_x, self.pos_y)
 
     def move(self):
 
@@ -83,16 +92,16 @@ class Player(pygame.sprite.Sprite):
             if self.rect.right > SCREEN_SIZE:
                 self.rect.move_ip(-SCREEN_SIZE, 0)
 
+
 if __name__ == "__main__":
-    Pac_man = Player()
+    Pac_man = Player(PACMAN_STARTING_POS_X, PACMAN_STARTING_POS_Y)
     dots = pygame.sprite.Group()
     all_sprites = pygame.sprite.Group()
 
     # Create given number of dots on the screen
     for i in range(MAX_SCORE):
-        Score_Dot = DOT()
-        Score_Dot.rect.x = random.randint(DOT_NO_SPAWN_FRAME, SCREEN_SIZE - DOT_NO_SPAWN_FRAME)
-        Score_Dot.rect.y = random.randint(DOT_NO_SPAWN_FRAME, SCREEN_SIZE - DOT_NO_SPAWN_FRAME)
+        Score_Dot = Dot(DOT_NO_SPAWN_FRAME, (SCREEN_SIZE - DOT_NO_SPAWN_FRAME))
+
         dots.add(Score_Dot)
         all_sprites.add(Score_Dot)
 
@@ -104,9 +113,13 @@ if __name__ == "__main__":
                 pygame.quit()
                 sys.exit()
 
+        game_time = pygame.time.get_ticks()
+
         DISPLAYSURF.fill(BLACK)
         current_score = font_small.render(str(SCORE), True, WHITE)
+        current_time = font_small.render(str(round((game_time / 1000), 2)), True, WHITE)
         DISPLAYSURF.blit(current_score, (SCORE_POS, SCORE_POS))
+        DISPLAYSURF.blit(current_time, (TIME_POS, SCORE_POS))
 
         Pac_man.move()
 
@@ -116,11 +129,11 @@ if __name__ == "__main__":
             SCORE += 1
         all_sprites.draw(DISPLAYSURF)
 
-        if SCORE == MAX_SCORE:
+        if SCORE == MAX_SCORE or game_time > MAX_TIME:
             time.sleep(0.5)
 
             DISPLAYSURF.fill(BLACK)
-            DISPLAYSURF.blit(game_over, (game_over_pos_x, game_over_pos_y))
+            DISPLAYSURF.blit(game_over, (GAME_OVER_POS_X, GAME_OVER_POS_Y))
 
             pygame.display.update()
             for entity in all_sprites:
@@ -129,5 +142,6 @@ if __name__ == "__main__":
             pygame.quit()
             sys.exit()
 
-    pygame.display.update()
-    FramePerSec.tick(FPS)
+        pygame.display.update()
+        FramePerSec.tick(FPS)
+
