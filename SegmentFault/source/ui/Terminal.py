49a542e788e0b0870b19deb302ae2d3b0caad203
markbozsoki <83731022+markbozsoki@users.noreply.github.com>
2022-10-29 13:50:27 +0200

    ADD: Terminal.py - input and output display functionalities

--- SegmentFault/source/ui/Terminal.py
+++ SegmentFault/source/ui/Terminal.py
@@ -17,22 +17,35 @@
 """
 
 import os
-from posixpath import relpath
+import InputParser, Inputs
 
 class Terminal():
 
     @staticmethod
-    def show_menu(menuitems: dict) -> None:
+    def show_menu(menutitle: str, menuitems: dict[str, str]) -> None:
+        """ Displays the given menu
+        @args:
+            menutitle [str] - the title of the current menu
+            menuitems [dict(str, str)] - dictionary containing the menuitems
+        """
+        print(menutitle)
+        for i in menuitems:
+            print(f"\t{i.key}. {i.value}")
 
-
-        pass
 
     @staticmethod
     def show_gameplayresult() -> None:
+        """ Displayes the gameplay results
+        @args:
+
+        """
         pass
-    
+
+
     @staticmethod
     def show_title() -> None:
+        """ Displayes the gametitle from local file
+        """
         current_dir = os.path.dirname(__file__)
         title_source_path = os.path.relpath("..\\data\\assets\\TerminalTitle.txt", current_dir)
         title = open(title_source_path, 'r')
@@ -48,12 +61,42 @@ class Terminal():
 
 
     @staticmethod
-    def clear():
+    def clear() -> None:
+        """ Clears the console
+        """
         command = 'clear'
-        if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        if os.name in ('nt', 'dos'):
             command = 'cls'
         os.system(command)
 
     @staticmethod
-    def get_input() -> str:
-        pass
+    def get_menu_input(massage: str, menuitems: dict[str, str]) -> str:
+        """ Returns the parsed input from the user for menu contol
+
+        @args:
+            massage [str] - displayed massage for user
+            menuitems [dict(str, str)] - dictionary containing the menuitems
+        @return:
+            parsed_input [str] - response from user
+        """
+        parsed_input = InputParser.parse(input(f"{massage}\n"), menuitems.keys())
+
+        if parsed_input is Inputs.Nothing:
+            parsed_input = InputParser.parse(input(f"{massage}\n"), menuitems.values())
+
+        return parsed_input
+
+    @staticmethod
+    def get_gameplay_input(massage: str) -> str:
+        """ Returns the parsed input from the user for gameplay control
+
+        @args:
+            massage [str] - displayed massage for user
+        @return:
+            parsed_input [str] - response from user
+        """
+        parsed_input = InputParser.parse(input(f"{massage}\n"))
+
+        return parsed_input
+
+    
