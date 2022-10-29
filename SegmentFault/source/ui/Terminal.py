93360a60c90d54e2fc3972661841b48bff6a861d
markbozsoki <83731022+markbozsoki@users.noreply.github.com>
2022-10-29 12:47:44 +0200

    ADD: Terminal.py - show_title()

--- SegmentFault/source/ui/Terminal.py
+++ SegmentFault/source/ui/Terminal.py
@@ -16,5 +16,44 @@
 /*********************************************************************
 """
 
+import os
+from posixpath import relpath
+
 class Terminal():
-    pass
+
+    @staticmethod
+    def show_menu(menuitems: dict) -> None:
+
+
+        pass
+
+    @staticmethod
+    def show_gameplayresult() -> None:
+        pass
+    
+    @staticmethod
+    def show_title() -> None:
+        current_dir = os.path.dirname(__file__)
+        title_source_path = os.path.relpath("..\\data\\assets\\TerminalTitle.txt", current_dir)
+        title = open(title_source_path, 'r')
+
+        while True:
+            line = title.readline()
+
+            # EndOfFile
+            if not line:
+                break
+
+            print(line)
+
+
+    @staticmethod
+    def clear():
+        command = 'clear'
+        if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+            command = 'cls'
+        os.system(command)
+
+    @staticmethod
+    def get_input() -> str:
+        pass
