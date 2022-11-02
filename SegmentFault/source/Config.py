4b4be0ef85666f798f5ccee5023dae591b4ff35b
Amontobix <juhaszpallorand@edu.bme.hu>
2022-11-02 23:53:18 +0100

    ADD: Comments

--- SegmentFault/source/Config.py
+++ SegmentFault/source/Config.py
@@ -32,10 +32,18 @@ import argparse
 
 
 class Config():
+    
     def __init__(self) -> None:
         self.Arguments = argparse.ArgumentParser(prog = "SegmentFault's Pacman")
         self.AddArguments()
         self.GivenArguments = self.Arguments.parse_args()
+
+    """ Initializes the flag with the help of argparse module
+    @args:
+        self
+    @return:
+            None
+    """
     def AddArguments(self) -> None:
         self.Arguments.add_argument("--gamemode", "-gm", action='store',
                                     help="Determins game type, standard/sandbox ", default="standard")
@@ -47,18 +55,47 @@ class Config():
                                     default="Basic.mapdat")
         self.Arguments.add_argument("--gamespeed", "-gs", help="Value of the gamespeed", default=1)
 
+
+    """ Returns the value of the given gamemode flag
+    @args:
+        self
+
+    @return:
+            gamemode [str]
+    """
     def GetGamemode(self) -> str:
         gamemode = self.GivenArguments.gamemode
         return gamemode
 
-    def GetTimeout(self) -> int:
-        Timeout = self.GivenArguments.timeout
-        return int(Timeout)
+    """ Returns the value of the given timeout flag
+    @args:
+        self
 
+    @return:
+            Timeout[int]
+    """
+    def GetTimeout(self) -> int:
+        Timeoutlimit = self.GivenArguments.timeout
+        return int(Timeoutlimit)
+
+    """ Returns the value of the given mapdata flag
+        @args:
+            self
+
+        @return:
+                Mapdatafile[str]
+    """
     def GetMapdata(self) -> str:
         Mapdatafile = self.GivenArguments.mapdata
         return Mapdatafile
 
+    """ Returns the value of the given GameSpeed flag
+    @args:
+        self
+
+    @return:
+            GameSpeed[int]
+    """
     def GetGamespeed(self) -> int:
         GameSpeed = self.GivenArguments.gamespeed
         return int(GameSpeed)
