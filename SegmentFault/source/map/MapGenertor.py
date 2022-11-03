d2876022409054c0a7b5a870e0fbcb717f0c80b2
origergely <origergelylaszlo@gmail.com>
2022-11-03 13:14:58 +0100

    ADD: MapGenertor.py - init method and translate_dataset method translate string database to map element's coordinate lists

--- SegmentFault/source/map/MapGenertor.py
+++ SegmentFault/source/map/MapGenertor.py
@@ -3,7 +3,7 @@
 //*********************************************************************
 / PROJECT_NAME : PacMan
 / FILENAME     : MapGenerator.py
-/ AUTHOR       : 
+/ AUTHOR       : Gergely Őri
 / UNIVERSITY   : BME
 / TEAM         : SegmentFault
 **********************************************************************
@@ -16,7 +16,27 @@
 /*********************************************************************
 """
 
-from source.map import MapElements
+from MapElements import MapElements
+import numpy as np
 
 class MapGenerator():
-    pass
+    def __init__(self, dataset: list[list[str]]):
+        self.dataset=dataset
+        self.mapelements=MapElements.list_value()
+        self.element_names=MapElements.list_name()
+        self.element_coordinates=[[]for element_count in range(len(self.mapelements))]
+        self.translate_dataset()
+    def translate_dataset(self):
+         for map_row in range(len(self.dataset)):
+             for map_column in range(len(self.dataset[map_row])):
+                 for element_count in range(len(self.mapelements)):
+                    if self.dataset[map_row][map_column]==self.mapelements[element_count]:
+                     #print(self.element_names[k])
+                     self.element_coordinates[element_count].append([map_row,map_column])
+
+
+
+if __name__ == "__main__":
+    db = ["o-#", "o-#", "o-#"]
+    trial = MapGenerator(db)
+    print(trial.element_coordinates)
