b076330f86b84858c7f0f6e759626a22cfdf6753
BPelenczei <81440132+BPelenczei@users.noreply.github.com>
2022-11-05 20:32:36 +0100

    ADD DefeatChecker class and abstractmethod skeleton functions

--- AgiliTEAM/solid_version/core/defeat_checker.py
+++ AgiliTEAM/solid_version/core/defeat_checker.py
@@ -2,3 +2,11 @@ from bosch_ASDIIE.AgiliTEAM.solid_version.core.game_element import GameElement
 from bosch_ASDIIE.AgiliTEAM.solid_version.core.key_event import KeyEvent
 from bosch_ASDIIE.AgiliTEAM.solid_version.core.ghosts import Ghosts
 from bosch_ASDIIE.AgiliTEAM.solid_version.core.pacman import Pacman
+
+
+class DefeatChecker(GameElement):
+    def take_action(self, key_event: KeyEvent):
+        pass
+
+    def tick(self) -> bool:
+        pass
