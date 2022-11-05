8b8d4be9798ebffeaf4a6908c9bc6be976b901d5
BPelenczei <81440132+BPelenczei@users.noreply.github.com>
2022-11-05 19:55:29 +0100

    ADD move.py file transferring the MovingTransformation class

---
+++ AgiliTEAM/solid_version/core/move.py
@@ -0,0 +1,23 @@
+class MovingTransformation:
+    """
+    A class for handling keyboard events during playing
+    """
+    def __init__(self, direction: KeyEvent, map_size: MapSize):
+        self.direction = direction
+        self.map_size = map_size
+
+    def __call__(self, coordinates: Coordinates) -> Coordinates:
+        if self.direction == KeyEvent.UP:
+            new_row = (coordinates.row - 1) % self.map_size.row_num
+            return Coordinates(new_row, coordinates.col)
+        elif self.direction == KeyEvent.LEFT:
+            new_col = (coordinates.col - 1) % self.map_size.col_num
+            return Coordinates(coordinates.row, new_col)
+        elif self.direction == KeyEvent.DOWN:
+            new_row = (coordinates.row + 1) % self.map_size.row_num
+            return Coordinates(new_row, coordinates.col)
+        elif self.direction == KeyEvent.RIGHT:
+            new_col = (coordinates.col + 1) % self.map_size.col_num
+            return Coordinates(coordinates.row, new_col)
+        else:
+            raise ValueError(f"There is no moving forward {self.direction} direction.")
