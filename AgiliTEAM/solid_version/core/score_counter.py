ea9041205b861278f28ed7dbef1f74281d93dba0
BPelenczei <81440132+BPelenczei@users.noreply.github.com>
2022-11-05 20:25:27 +0100

    ADD ScoreCounter class and abstractmethod skeleton functions

--- AgiliTEAM/solid_version/core/score_counter.py
+++ AgiliTEAM/solid_version/core/score_counter.py
@@ -2,3 +2,11 @@ from bosch_ASDIIE.AgiliTEAM.solid_version.core.game_element import GameElement
 from bosch_ASDIIE.AgiliTEAM.solid_version.core.key_event import KeyEvent
 from bosch_ASDIIE.AgiliTEAM.solid_version.core.pellets import Pellets
 from bosch_ASDIIE.AgiliTEAM.solid_version.core.pacman import Pacman
+
+
+class ScoreCounter(GameElement):
+    def take_action(self, key_event: KeyEvent):
+        pass
+
+    def tick(self) -> bool:
+        pass
