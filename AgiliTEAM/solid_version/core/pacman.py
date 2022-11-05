69b1ccd96ac272aaf5b68bfeb0e95320cad2d141
BPelenczei <81440132+BPelenczei@users.noreply.github.com>
2022-11-05 19:51:53 +0100

    ADD implementation of position generator in pacman.py

--- AgiliTEAM/solid_version/core/pacman.py
+++ AgiliTEAM/solid_version/core/pacman.py
@@ -32,6 +32,12 @@ class Pacman(GameElement, Visualizable):
 
         self.moving_transformation = MovingTransformation(starting_direction, map_size)
 
+    def generate_pos(self, num_of_pos: int, map_size: MapSize) -> List[Coordinates]:
+        pos = Coordinates(np.random.randint(map_size[0]), np.random.randint(map_size[1]))
+        while pos in self.known_pos:
+            pos = Coordinates(np.random.randint(map_size[0]), np.random.randint(map_size[1]))
+        return [pos]
+
     def take_action(self, key_event: KeyEvent):
         if self._is_not_opposite_direction(key_event):
             self.moving_transformation.direction = key_event
