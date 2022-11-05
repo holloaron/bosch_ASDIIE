1d31bb485a6f63a08d44122bbed6f6d9cbfadd51
BPelenczei <81440132+BPelenczei@users.noreply.github.com>
2022-11-05 19:44:06 +0100

    CHANGE module names in game.py

--- AgiliTEAM/solid_version/core/game.py
+++ AgiliTEAM/solid_version/core/game.py
@@ -1,8 +1,8 @@
 import time
 
-from bosch_ASDIIE.solid_version.core.key_listener import KeyboardListener
-from bosch_ASDIIE.solid_version.core.snake_game_state import SnakeGameState
-from bosch_ASDIIE.solid_version.core.visualizer import Visualizer
+from bosch_ASDIIE.AgiliTEAM.solid_version.core.key_listener import KeyListener
+from bosch_ASDIIE.AgiliTEAM.solid_version.core.pacman_game_state import PacmanGameState
+from bosch_ASDIIE.AgiliTEAM.solid_version.core.visualizer import Visualizer
 
 
 class Game:
