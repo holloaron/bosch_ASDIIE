b459bfce98c8ebcb27bd2cf7a06df5042b99aecb
BPelenczei <81440132+BPelenczei@users.noreply.github.com>
2022-11-05 20:31:47 +0100

    ADD module imports to defeat_checker.py

---
+++ AgiliTEAM/solid_version/core/defeat_checker.py
@@ -0,0 +1,4 @@
+from bosch_ASDIIE.AgiliTEAM.solid_version.core.game_element import GameElement
+from bosch_ASDIIE.AgiliTEAM.solid_version.core.key_event import KeyEvent
+from bosch_ASDIIE.AgiliTEAM.solid_version.core.ghosts import Ghosts
+from bosch_ASDIIE.AgiliTEAM.solid_version.core.pacman import Pacman
