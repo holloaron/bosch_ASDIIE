e033c4db41a51468e00614d9c58fbdb8ea9548fc
BPelenczei <81440132+BPelenczei@users.noreply.github.com>
2022-11-05 20:05:06 +0100

    ADD implementation of constructor function in Pellets class

--- AgiliTEAM/solid_version/core/pellets.py
+++ AgiliTEAM/solid_version/core/pellets.py
@@ -15,6 +15,15 @@ class Pellets(GameElement, Visualizable):
                  num_pellets: int = 10,
                  known_pos: List[List[Coordinates]] = None,
                  ):
+        if known_pos is not None:
+            self.known_pos = [item for sublist in known_pos for item in sublist]
+        else:
+            self.known_pos = []
+
+        if map_size is None:
+            map_size = MapSize(10, 10)
+
+        self.pos = self.generate_pos(num_of_pos=num_pellets, map_size=map_size)
 
     def take_action(self, key_event: KeyEvent):
         pass
