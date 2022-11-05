68ae216d95e001bdc10923dd7232486e6e97aee7
origergely <origergelylaszlo@gmail.com>
2022-11-05 12:43:16 +0100

    ADD: MapGenerator.py - new functions "get_obstacle_coordinates", "get_mapsize", "contains_obstacle" to serve MapDadataReader classes functions

--- SegmentFault/source/map/MapGenerator.py
+++ SegmentFault/source/map/MapGenerator.py
@@ -15,6 +15,7 @@
 /*********************************************************************
 /*********************************************************************
 """
+from typing import List
 
 from MapElements import MapElements
 
@@ -40,6 +41,10 @@ class MapGenerator:
     def get_coordinates(self):
         return self.element_coordinates
 
+    def get_obsacle_coordinates(self, obstacle: MapElements) ->list[tuple[int,int]]:
+        if obstacle.name in self.element_names:
+            return self.element_coordinates[self.element_names.index(obstacle.name)]
+
     def generate_basic_map(self):
         first_and_last_row = MapElements.Wall.value * 27
         inner_row = MapElements.Wall.value
@@ -48,6 +53,16 @@ class MapGenerator:
         self.dataset = [first_and_last_row, inner_row,first_and_last_row]
         for added_row in range(len(first_and_last_row)-3):
             self.dataset.insert(1,inner_row)
+
+    def get_mapsize(self)-> list[int]:
+        return [len(self.dataset[0]), len(self.dataset)]
+
+    def contains_obstacle(self, obstacle:MapElements)->bool:
+        for row_index in range(len(self.dataset)):
+            if obstacle.value in self.dataset[row_index]:
+                return True
+
+        return False
 
 
 
