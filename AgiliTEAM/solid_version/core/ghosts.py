0c7a004ce4389daca4965a20525f78a1c9beec8b
BPelenczei <81440132+BPelenczei@users.noreply.github.com>
2022-11-05 20:12:49 +0100

    ADD module imports to ghosts.py

---
+++ AgiliTEAM/solid_version/core/ghosts.py
@@ -0,0 +1,8 @@
+from typing import List
+import numpy as np
+
+from bosch_ASDIIE.AgiliTEAM.solid_version.core.key_event import KeyEvent
+from bosch_ASDIIE.AgiliTEAM.solid_version.core.game_element import GameElement
+from bosch_ASDIIE.AgiliTEAM.solid_version.core.visualizable import Visualizable
+from bosch_ASDIIE.AgiliTEAM.solid_version.core.canvas import Canvas
+from bosch_ASDIIE.AgiliTEAM.solid_version.core.map import MapSize, Coordinates
