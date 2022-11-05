daa4cc5c135068fb9cd9a4fb1adba6a7724acb09
BPelenczei <81440132+BPelenczei@users.noreply.github.com>
2022-11-05 20:13:58 +0100

    ADD Ghosts class and abstractmethod skeleton functions to ghosts.py

--- AgiliTEAM/solid_version/core/ghosts.py
+++ AgiliTEAM/solid_version/core/ghosts.py
@@ -6,3 +6,15 @@ from bosch_ASDIIE.AgiliTEAM.solid_version.core.game_element import GameElement
 from bosch_ASDIIE.AgiliTEAM.solid_version.core.visualizable import Visualizable
 from bosch_ASDIIE.AgiliTEAM.solid_version.core.canvas import Canvas
 from bosch_ASDIIE.AgiliTEAM.solid_version.core.map import MapSize, Coordinates
+
+
+class Ghosts(GameElement, Visualizable):
+
+    def take_action(self, key_event: KeyEvent):
+        pass
+
+    def tick(self) -> bool:
+        pass
+
+    def draw(self, canvas: Canvas):
+        pass
