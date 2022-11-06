9218fa18aaf70194d57894f4a0b57bce92d108d6
gujgidani <gujgiczerd@edu.bme.hu>
2022-11-06 19:59:42 +0100

    Import needed modules for Visualizer class

---
+++ Git_egylet/solid_version/core/visualizer.py
@@ -0,0 +1,4 @@
+from typing import List
+
+from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.canvas import Canvas
+from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.visualizable import Visualizable
