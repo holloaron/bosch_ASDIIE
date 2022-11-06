2aeae4db97c7604f30c1a02369eebd55095422c1
istvan-knab <97619115+istvan-knab@users.noreply.github.com>
2022-11-06 11:33:38 +0100

    ADD FEATURE: implement gui

---
+++ I_NEED_A/pacman_solid/Objects/gui/console_canvas.py
@@ -0,0 +1,50 @@
+import os
+from typing import List
+
+from Objects.core.map import Coordinates, MapSize
+from Objects.core.canvas import Canvas
+from Objects.core.screen import Screen
+
+
+class ConsoleCanvas(Canvas):
+    """
+    The canvas class for the console version
+    """
+
+    def __init__(self,
+                 map_size: MapSize,
+                 curses_screen: Screen):
+
+        self.width = map_size.col_num + 1
+        self.height = map_size.col_num + 1
+        self.crs_screen = curses_screen
+        self.map = self._get_empty_map()
+
+    def clear(self):
+        command = 'clear'
+        if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+            command = 'cls'
+        os.system(command)
+        self.map = self._get_empty_map()
+        self.crs_screen.clear()
+
+    def draw_dots(self, coordinates: List[Coordinates]):
+        for dot in coordinates:
+            self.map[dot.row][dot.col] = "x"
+
+    def render(self):
+        for num_row, row in enumerate(self.map):
+            self.crs_screen.addstr(num_row, 0, "".join(row) + "\n")
+        self.crs_screen.refresh()
+
+    def _get_empty_map(self):
+        screen = []
+        for _ in range(self.height):
+            screen.append([" "] * self.width)
+        return screen
+
+    def get_height(self):
+        return self.height
+
+    def get_width(self):
+        return self.width
--- I_NEED_A/pacman_solid/pacman_run.py
+++ I_NEED_A/pacman_solid/pacman_run.py
@@ -1,12 +1,45 @@
-#TODO:import other classes
-#TODO:import packages
+import curses
+from argparse import ArgumentParser
+
+from Objects.core.game import Game
+from Objects.core.get_action import KeyEvent
+from Objects.core.map import MapSize
+from Objects.core.screen import Screen
+from Objects.core.visualizer import Visualizer
+#TODO
+from Objects.core.pacman_game_state import
+from Objects.core.pacman import
+from Objects.gui.console_canvas import
+
+
+WIDTH = 20
+HEIGHT = 20
+
+
+def main():
+    # arg parser
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument("--gui", type=str, default="console")
+    args = arg_parser.parse_args()
+
+    # using curses lib for proper keyboard-canvas interaction through SSH
+    screen = Screen()
+    curses.cbreak()
+
+    key_listener = KeyboardListener()
+    key_listener.start(screen)
+
+    snake = Snake(map_size=MapSize(HEIGHT, WIDTH))
+    visualizer = Visualizer([snake], ConsoleCanvas(MapSize(HEIGHT, WIDTH), screen))
+    start_game_state = SnakeGameState([snake])
+    game = Game(key_listener, start_game_state, visualizer)
+    game.run()
+
+    # stop the screen
+    curses.nocbreak()
+    screen.keypad(0)
+    curses.echo()
+    curses.endwin()
+
 if __name__ == "__main__":
-
-    while(1):
-
-        #TODO:represent choosing an action
-
-        #TODO:represent step
-
-        #TODO:Realize render
-        break
+    main()
