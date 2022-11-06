43d920803d58218aec96b244d523c3945f358b7a
Buxi527bme <102481697+Buxi527bme@users.noreply.github.com>
2022-11-06 21:35:05 +0100

    Added return function and docstring

--- Namco/position_comparison.py
+++ Namco/position_comparison.py
@@ -3,19 +3,20 @@ import os
 
 
 class PositionComparison:
-    def check_collision(self) -> None:
+    def check_collision(self) -> [bool,bool]:
         """
         Checks whether Pacman picked up a dot and increases the score accordingly
-        :return: -
+        :return: wall collision flag (bool), dot collision flag (bool)
         """
         for dot in self.dots:
             if dot == tuple(self.pacman):
                 # If Pacman moved on a dot, increase the score and remove the dot
-                self.score += 1
+                self.dot_collision
                 self.dots.remove(dot)
 
         for wall in self.wall:
             if wall == tuple(self.pacman):
                 # If Pacman moved on a wall, terminate game
-                self.wall_hit = true
+                self.wall_collision = true
 
+        return self.wall_collision, self.dot_collision
