1efa4476ac231f851d0077a85fe525dee0436c65
gujgidani <gujgiczerd@edu.bme.hu>
2022-11-06 22:04:31 +0100

    Add KeyListener class
    Defined the implement function of the class.

--- Git_egylet/solid_version/core/key_listener.py
+++ Git_egylet/solid_version/core/key_listener.py
@@ -1,3 +1,11 @@
 import threading
 
 from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.key_event import KeyEvent
+
+
+class KeyListener:
+    def __init__(self):
+        self.screen = None
+        self.last_key = None
+        self.thread = None
+        self.stopped = False
