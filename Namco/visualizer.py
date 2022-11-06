c46789b951f30d992c5fababf441149c37570075
farkas-peter <peterfarkas@edu.bme.hu>
2022-11-06 21:09:01 +0100

    Init version of visualizer.py

--- Namco/visualizer.py
+++ Namco/visualizer.py
@@ -1,21 +1,24 @@
 import os
 
+import numpy as np
+
 
 class Visualizer:
-    def __init__(self):
-        self.observation = None
-
     def clear_console(self):
-        # Clearing console before printing the map (cannot clear the console in PyCharm, only in terminal)
+        """
+        Clearing the console
+        :return:
+        """
+        # Note: Cannot clear the console in PyCharm, only in terminal
         os.system('cls' if os.name == 'nt' else 'clear')
 
-    def process_observation(self, observation):
-        # Converting numpy array to python list
-        self.observation = observation.tolist()
-
-    def render(self):
-        # Printing the current score and the current observation line by line with separation
-        for line in self.observation:
+    def render(self, observation: np.ndarray):
+        """
+        Visualizing the current state of the game to the player
+        :param observation: Array containing the map (np.ndarray)
+        :return:
+        """
+        for line in observation:
             print(*line, sep='  ')
 
     def show_score(self, curr_score: int):
