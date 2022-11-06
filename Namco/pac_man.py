062ffd941321499459083a280305044469f08127
farkas-peter <peterfarkas@edu.bme.hu>
2022-11-06 21:10:46 +0100

    Init version of pac_man.py

--- Namco/pac_man.py
+++ Namco/pac_man.py
@@ -1,9 +1,12 @@
 class PacMan:
     def __init__(self):
+        """
+        Implements PacMan
+        """
         self.row = 0
         self.col = 0
 
-    def process_action(self, action: str) -> None:
+    def process_action(self, action: str) -> [int, int]:
         """
         Moves PacMan in the desired direction
         :param action: current user input (w, a, s or d) (str)
@@ -21,3 +24,5 @@ class PacMan:
         # Moving right
         elif action == 'd':
             self.col += 1
+
+        return self.row, self.col
