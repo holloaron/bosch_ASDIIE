a8aee426d783a2fffce0bf37364a38f99498ed57
gujgidani <gujgiczerd@edu.bme.hu>
2022-11-06 22:22:23 +0100

    Import needed modules for PacmanGameState class

---
+++ Git_egylet/solid_version/core/pacman_game_state.py
@@ -0,0 +1,4 @@
+from typing import List
+
+from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.game_element import GameElement
+from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.key_event import KeyEvent
