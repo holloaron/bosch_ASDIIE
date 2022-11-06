9de64a47bd478c1ca14f82fe6cc710f2405853a6
gujgidani <gujgiczerd@edu.bme.hu>
2022-11-06 22:24:35 +0100

    Add PacmanGameState class
    Added PacmanGameState class which gets the current state of the game, handles the actions and checks if it's terminated,

--- Git_egylet/solid_version/core/pacman_game_state.py
+++ Git_egylet/solid_version/core/pacman_game_state.py
@@ -2,3 +2,9 @@ from typing import List
 
 from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.game_element import GameElement
 from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.key_event import KeyEvent
+
+
+class PacmanGameState:
+    def __init__(self, game_elements: List[GameElement]):
+        self.game_elements = game_elements
+        self._can_game_continue = True
