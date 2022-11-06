77e4b0ce70847e36e3e2b7be7d8da52486e7bf97
Kurtiadam <adamkurti97@gmail.com>
2022-11-06 20:43:33 +0100

    ADD: New Pacman map from based on a .txt file

--- Namco/map.py
+++ Namco/map.py
@@ -2,18 +2,22 @@ import numpy as np
 
 class Map:
     def __init__(self):
+        """
+        Map class: initializing the map with walls and dots
+        """
         # Loading map from the .txt file
+        self.updated_map = None
         self.empty_map = np.loadtxt("map.txt", dtype= str, delimiter=',')
         # Filling up the map with dots
-        self.filled_map= self.empty_map[self.empty_map == ' '] = 'o'
-        # for line in self.empty_map:
-        #    print(*line, sep='  ')
-    def return_map (self):
+        self.empty_map[self.empty_map == ' '] = 'o'
+        self.filled_map = self.empty_map
+
+    def update_map (self,x: int,y: int) -> np.ndarray:
         """
         Updates the map based on Pacman's position
-        :return: updated map (list(list(str)))
+        :return: updated map (np.ndarray)
         """
-        return self.filled_map
-
-env = Map()
-filled_map = env.return_map()
+        self.filled_map[self.filled_map == '0'] = ' '
+        self.filled_map[x][y] = '0'
+        self.updated_map = self.filled_map
+        return self.updated_map
