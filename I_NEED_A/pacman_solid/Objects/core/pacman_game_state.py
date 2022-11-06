20df64cc28c6d51ccbc871d2a6a10f7dae2eacab
istvan-knab <97619115+istvan-knab@users.noreply.github.com>
2022-11-06 11:35:33 +0100

    ADD FEATURE: implement state

---
+++ I_NEED_A/pacman_solid/Objects/core/pacman_game_state.py
@@ -0,0 +1,24 @@
+from typing import List
+
+from Objects.core.game_element import GameElement
+from Objects.core.get_action import KeyEvent
+
+
+class SnakeGameState:
+    """
+    A class for managing the game state, handling actions of the game elements
+    """
+    def __init__(self, game_elements: List[GameElement]):
+        self.game_elements = game_elements
+        self._can_game_continue = True
+
+    def step(self):
+        for game_element in self.game_elements:
+            self._can_game_continue = self._can_game_continue and game_element.tick()
+
+    def is_terminated(self):
+        return not self._can_game_continue
+
+    def take_action(self, key_event: KeyEvent):
+        for game_element in self.game_elements:
+            game_element.take_action(key_event)
