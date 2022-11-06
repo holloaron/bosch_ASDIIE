3dd5f4cb075e0d8ae6a11b99074a5cf8b2e5def0
gujgidani <gujgiczerd@edu.bme.hu>
2022-11-06 22:05:46 +0100

    Add variables to KeyListener class

--- Git_egylet/solid_version/core/key_listener.py
+++ Git_egylet/solid_version/core/key_listener.py
@@ -4,6 +4,14 @@ from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.key_event import KeyE
 
 
 class KeyListener:
+    KEYPRESS_TO_KEY_EVENT = {
+        "KEY_UP": KeyEvent.UP,
+        "KEY_DOWN": KeyEvent.DOWN,
+        "KEY_LEFT": KeyEvent.LEFT,
+        "KEY_RIGHT": KeyEvent.RIGHT,
+    }
+    MILLISECONDS = 1000
+    
     def __init__(self):
         self.screen = None
         self.last_key = None
