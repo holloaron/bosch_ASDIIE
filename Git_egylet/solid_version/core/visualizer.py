193dbcdb3207f9e248d4f85dcdb673a864c5dff9
gujgidani <gujgiczerd@edu.bme.hu>
2022-11-06 20:02:43 +0100

    Add Visualizer class
    Added Visualizer class for visualizing different objects.

--- Git_egylet/solid_version/core/visualizer.py
+++ Git_egylet/solid_version/core/visualizer.py
@@ -2,3 +2,15 @@ from typing import List
 
 from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.canvas import Canvas
 from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.visualizable import Visualizable
+
+
+class Visualizer:
+    def __init__(self, visualizable_objects: List[Visualizable], canvas: Canvas):
+        self.visualizable_objects = visualizable_objects
+        self.canvas = canvas
+
+    def render(self):
+        self.canvas.clear()
+        for obj in self.visualizable_objects:
+            obj.draw(self.canvas)
+        self.canvas.render()
