bf8fe128a4c9e30bd5a2fc6916c5cfcddb05036e
gujgidani <gujgiczerd@edu.bme.hu>
2022-11-06 19:57:34 +0100

    Add Visualizable class
    Created Visualizable class for visualizing different objects in the game.

--- Git_egylet/solid_version/core/visualizable.py
+++ Git_egylet/solid_version/core/visualizable.py
@@ -1,3 +1,9 @@
 from abc import ABC, abstractmethod
 
 from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.canvas import Canvas
+
+
+class Visualizable(ABC):
+    @abstractmethod
+    def draw(self, canvas: Canvas):
+        pass
