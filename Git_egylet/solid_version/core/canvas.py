bcd26a823758efd4822542ed409563ca64c37133
gujgidani <gujgiczerd@edu.bme.hu>
2022-11-06 19:43:59 +0100

    Add Canvas class

--- Git_egylet/solid_version/core/canvas.py
+++ Git_egylet/solid_version/core/canvas.py
@@ -2,3 +2,25 @@ from abc import ABC, abstractmethod
 from typing import List
 
 from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.map import Coordinates
+
+
+class Canvas(ABC):
+    @abstractmethod
+    def draw_dots(self, coordinates: List[Coordinates]):
+        pass
+
+    @abstractmethod
+    def render(self):
+        pass
+
+    @abstractmethod
+    def clear(self):
+        pass
+
+    @abstractmethod
+    def get_height(self):
+        pass
+
+    @abstractmethod
+    def get_width(self):
+        pass
