6e2fdebbaa6f5d9766f6eeeda37b706f41f751be
gujgidani <gujgiczerd@edu.bme.hu>
2022-11-06 20:13:51 +0100

    Import needed modules for the GUI

---
+++ Git_egylet/solid_version/gui/console_canvas.py
@@ -0,0 +1,6 @@
+import os
+from typing import List
+
+from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.map import Coordinates, MapSize
+from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.canvas import Canvas
+from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.screen import Screen
