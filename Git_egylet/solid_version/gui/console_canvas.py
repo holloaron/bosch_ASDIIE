93c3c229feada1bbc3e6a016438661a9354c505c
gujgidani <gujgiczerd@edu.bme.hu>
2022-11-06 20:17:42 +0100

    Add ConsoleCanvas class
    Defined the initializer function

--- Git_egylet/solid_version/gui/console_canvas.py
+++ Git_egylet/solid_version/gui/console_canvas.py
@@ -4,3 +4,11 @@ from typing import List
 from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.map import Coordinates, MapSize
 from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.canvas import Canvas
 from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.screen import Screen
+
+
+class ConsoleCanvas(Canvas):
+    def __init__(self, map_size: MapSize, curses_screen: Screen):
+        self.width = map_size.col_num + 1
+        self.height = map_size.col_num + 1
+        self.crs_screen = curses_screen
+        self.map = self._get_empty_map()
