68579736afd33c702b88b2967bfb2ccb2014d7bf
Kurtiadam <adamkurti97@gmail.com>
2022-11-06 21:51:06 +0100

    CHG: Simplifying map names, cosmetic changes

--- Namco/map.py
+++ Namco/map.py
@@ -1,23 +1,25 @@
 import numpy as np
 
+
 class Map:
-    def __init__(self):
+    def __init__(self, map_source: str = "map.txt"):
         """
+        :param map_source: name of the map file (str)
         Map class: initializing the map with walls and dots
         """
         # Loading map from the .txt file
-        self.updated_map = None
-        self.empty_map = np.loadtxt("map.txt", dtype= str, delimiter=',')
+        self.map = None
+        self.map = np.loadtxt(map_source, dtype=str, delimiter=',')
         # Filling up the map with dots
-        self.empty_map[self.empty_map == ' '] = 'o'
-        self.filled_map = self.empty_map
+        self.map[self.map == ' '] = 'x'
 
-    def update_map (self,x: int,y: int) -> np.ndarray:
+    def update_map(self, x: int, y: int) -> np.ndarray:
         """
         Updates the map based on Pacman's position
+        :param x: Pacman's x position (int)
+        :param y: Pacman's y position (int)
         :return: updated map (np.ndarray)
         """
-        self.filled_map[self.filled_map == '0'] = ' '
-        self.filled_map[x][y] = '0'
-        self.updated_map = self.filled_map
-        return self.updated_map
+        self.map[self.map == '0'] = ' '
+        self.map[x][y] = '0'
+        return self.map
