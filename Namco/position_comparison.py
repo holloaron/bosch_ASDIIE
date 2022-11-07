159889bccae5f84ee90f3e295236b7d4cce71b74
Buxi527bme <102481697+Buxi527bme@users.noreply.github.com>
2022-11-07 16:16:12 +0100

    Empty lines deleted

--- Namco/position_comparison.py
+++ Namco/position_comparison.py
@@ -4,8 +4,7 @@ import os
 
 class PositionComparison:
 
-
-    def check_collision(self, map, pacman_x: int, pacman_y: int) -> [bool,bool]:
+    def check_collision(self, map, pacman_x: int, pacman_y: int) -> [bool, bool]:
         """
         Checks whether Pacman picked up a dot or hit a wall, changes flags accordingly
         :return: wall collision flag (bool), dot collision flag (bool)
@@ -18,6 +17,5 @@ class PositionComparison:
 
         elif map[pacman_x][pacman_y] == 'x':
             dot_collision = True
-            
 
-        return self.wall_collision, self.dot_collision
+        return wall_collision, dot_collision
