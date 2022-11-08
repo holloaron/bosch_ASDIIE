e19443a7543930ef0cc57904de6bc22268e52a5f
benceuveges <103946807+benceuveges@users.noreply.github.com>
2022-11-08 01:53:29 +0100

    Add MovingTransformation class

--- Git_egylet/solid_version/core/moving_transformation.py
+++ Git_egylet/solid_version/core/moving_transformation.py
@@ -1,2 +1,26 @@
 from bosch_ASDIIE_Git_egylet.solid_version.core.key_event import KeyEvent
 from bosch_ASDIIE_Git_egylet.solid_version.core.map import Coordinates, MapSize
+
+class MovingTransformation:
+    """
+    This class is for handling keyboard events during playing
+    """
+    def __init__(self, direction: KeyEvent, map_size: MapSize):
+        self.direction = direction
+        self.map_size = map_size
+
+    def __call__(self, coordinates: Coordinates) -> Coordinates:
+        if self.direction == KeyEvent.UP:
+            new_row = (coordinates.row - 1) % self.map_size.row_num
+            return Coordinates(new_row, coordinates.col)
+        elif self.direction == KeyEvent.LEFT:
+            new_col = (coordinates.col - 1) % self.map_size.col_num
+            return Coordinates(coordinates.row, new_col)
+        elif self.direction == KeyEvent.DOWN:
+            new_row = (coordinates.row + 1) % self.map_size.row_num
+            return Coordinates(new_row, coordinates.col)
+        elif self.direction == KeyEvent.RIGHT:
+            new_col = (coordinates.col + 1) % self.map_size.col_num
+            return Coordinates(coordinates.row, new_col)
+        else:
+            raise ValueError(f"There is no moving forward {self.direction} direction.")
