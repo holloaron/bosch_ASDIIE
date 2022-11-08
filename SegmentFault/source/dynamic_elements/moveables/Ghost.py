6ae795b0ebe30ad293ebf3cecddf66b2e229819f
markbozsoki <83731022+markbozsoki@users.noreply.github.com>
2022-11-08 15:30:31 +0100

    ADD: Ghost.py - base interaction logic documented and implemented

--- SegmentFault/source/dynamic_elements/moveables/Ghost.py
+++ SegmentFault/source/dynamic_elements/moveables/Ghost.py
@@ -17,51 +17,94 @@ Ghosts object definitions, movement and modes
 """
 
 import Moveable, Direction, GhostMode
+from source.map.MapData import MapData
 
-class Ghost(Moveable.Movable):
-    def __init__(self, start_position: tuple[int, int], start_diretcion: Direction) -> None:
-        super(Ghost, self).__init_()
+class Ghost(Moveable):
+    def __init__(self, mapdata: MapData, start_position: tuple[int, int]) -> None:
+        super().__init_()
+        self.mapdata = mapdata
         self.position = start_position
-        self.direction = start_diretcion
+        self.last_position = start_position
+        self.direction = self.generate_new_direction()
+        self.safehouse_position = self.get_safehouse_position()
+
         self.mode = GhostMode.Roam
-        self.root = None
+        self.path = []
+
+    def get_safehouse_position(self):
+        """ Calculates the first position on the map next to the Door
+            where the Player can't reach the Ghost
+        """
 
 
-    def Move(self):
+    def generate_new_direction(self):
+        """ Generates a random direction for the Ghost member based on mapdata
+        
+        """
+        
+
+
+    def set_next_position(self):
         """ Calculates the Ghost next position based on the GhostMode
         
         """
+        # goes around the map with no bad intention
         if self.mode == GhostMode.Roam:
-            pass
+            #TODO: calculate next position and direction
+            
+            #TODO: change Ghostmode as the Player visible and exit the if statment
+            
+            #TODO: append next position to path
+            self.path = []
+            self.path.append(0,0)
 
+        # if saw the player, goes to the last position where the player was seen
         if self.mode == GhostMode.Hunt:
-            pass
+            if self.is_PacMan_visible():
+                self.path = self.calculate_path_to(self.mapdata.Player)
 
+        # if the player eats a coin the Ghost goes to the safehouse position
         if self.mode == GhostMode.Fear:
-            pass
+            if self.path[-1] != self.safehouse_position:
+                self.path = self.calculate_path_to(self.safehouse_position)
+            
+            if self.position == self.safehouse_position:
+                self.mode = GhostMode.Roam
+
+        # moves to next position
+        self.position = self.path[0]
+        self.path.remove[0]
+
+        #TODO: refresh direction as the Ghost move on its path
 
     
-    def generate_new_direction(self):
-        """
-        
-        """
-
-        pass
-
-    
-    def is_PacMan_ahead(self) -> bool:
+    def is_PacMan_visible(self) -> bool:
         """ Determines if PacMan is visible for the Ghost
 
         @return:
             True, if PacMan is in the way of the Ghost
         """
-        test_x, test_y = self.get_next_position()
+        
+        test_pos_x = self.position[0]
+        test_pos_y = self.position[1]
+        test_position = (test_pos_x, test_pos_y)
 
-        while not self.is_wall_ahead(test_x, test_y):
-            
-            return True
+        while not super().is_wall_ahead(self.mapdata, test_position, self.direction):
+            test_position = super().next_position(test_position, self.direction)
+            if self.mapdata.Player == test_position:
+                return True
 
         return False
 
+    def calculate_path_to(self, destination: tuple[int, int]) -> list[tuple[int, int]]:
+        """ Calculates the shortest path from the current position to the destination
 
-    
+        @args:
+            destination [tuple(int, int)]
+        @returns:
+            path [list(tuple(int, int))]
+        """
+        self.path = []
+
+        #TODO: implement shortest path search
+
