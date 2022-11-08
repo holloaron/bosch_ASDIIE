af918044ae43ad97cfa95428c80958702620d6c2
Czuri Dávid <david.czuri@consteelsoftware.com>
2022-11-08 13:55:54 +0100

    Add score, pacman, gameover display

--- TeamSix/solid_version/drawingService.py
+++ TeamSix/solid_version/drawingService.py
@@ -15,3 +15,27 @@ class DrawingService:
         pygame.draw.rect(self.screen, color, pygame.Rect(tile_position_x * Constants.TILE_SIZE.value,
                                                          tile_position_y * Constants.TILE_SIZE.value,
                                                          Constants.TILE_SIZE.value, Constants.TILE_SIZE.value))
+
+    def draw_coin(self, tile_position_x, tile_position_y):
+        # x,y are the index of the field, multiplying with field size gives the top left coordinates on the screen
+        # convert it to the middle, since circle drawing requires that
+        circle_x = tile_position_x * Constants.TILE_SIZE.value + Constants.TILE_SIZE.value / 2
+        circle_y = tile_position_y * Constants.TILE_SIZE.value + Constants.TILE_SIZE.value / 2
+        pygame.draw.circle(self.screen, Constants.COLOR_WHITE.value, (circle_x, circle_y), Constants.COIN_SIZE.value)
+
+    def draw_pacman(self, tile_position_x, tile_position_y):
+        # x,y are the index of the field, multiplying with field size gives the top left coordinates on the screen
+        # convert it to the middle, since circle drawing requires that
+        circle_x = tile_position_x * Constants.TILE_SIZE.value + Constants.TILE_SIZE.value / 2
+        circle_y = tile_position_y * Constants.TILE_SIZE.value + Constants.TILE_SIZE.value / 2
+        pygame.draw.circle(self.screen, Constants.COLOR_YELLOW.value, (circle_x, circle_y), Constants.PACMAN_SIZE.value)
+
+    def draw_score(self, score):
+        font = pygame.font.SysFont('Comic Sans MS', 12)
+        text = font.render('Score: ' + str(score), False, Constants.COLOR_WHITE.value)
+        self.screen.blit(text, (5, 5))
+
+    def draw_game_over(self,):
+        font = pygame.font.SysFont('Comic Sans MS', 22)
+        text = font.render('Game over!', False, Constants.COLOR_WHITE.value)
+        self.screen.blit(text, (Constants.WINDOW_WIDTH.value/2-50, Constants.WINDOW_HEIGHT.value/2))
