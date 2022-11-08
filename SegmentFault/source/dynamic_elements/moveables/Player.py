f0c7610f650d809b6df9dbd405fced8b05cdf2ae
markbozsoki <83731022+markbozsoki@users.noreply.github.com>
2022-11-08 14:54:50 +0100

    CHG: Player.py - refactoring

--- SegmentFault/source/dynamic_elements/moveables/Player.py
+++ SegmentFault/source/dynamic_elements/moveables/Player.py
@@ -22,11 +22,15 @@ from source.map import MapData
 
 class Player(Moveable):
     def __init__(self, mapdata: MapData, start_position: tuple[int, int], start_direction: Direction):
-        super().___init__(mapdata, start_position, start_direction)
-        self.direction = start_direction
+        super().___init__()
+        self.mapdata = mapdata
         self.position = start_position
+        self.direction = start_direction
+        self.next_direction = None
+
         self.is_dead = False
         self.score = 0
+
 
     def get_next_position(self) -> tuple[int, int]:
         """ Sets the players new position based on the mapdata, current position and direction
@@ -34,36 +38,36 @@ class Player(Moveable):
         @return:
             (pos_x, pos_y) [int, int] - the player new position
         """
-        pos_x = self.position[0]
-        pos_y = self.position[1]
 
         # change the direction, if turning is available
-        if self.player.next_direction != None:
-            test_x, test_y = self.next_position(self.player.next_direction, pos_x, pos_y)
-            if not self.is_obstacle(test_x, test_y):
-                self.player.direction = self.player.next_direction
-                self.player.next_direction = None
+        if self.next_direction != None:
+            test_position = super().next_position(self.position, self.next_direction)
+            if not super().is_wall_ahead(self.mapdata, test_position, self.direction):
+                self.direction = self.next_direction
+                self.next_direction = None
         
         # calculate the next position
-        pos_x, pos_y = self.next_position(self.player.direction, pos_x, pos_y)
+        self.position = super().next_position(self.position, self.direction)
 
         # reset position if obstacle ahead
-        if self.is_obstacle(pos_x, pos_y):
-            pos_x, pos_y = self.reset_position(self.player.direction, pos_x, pos_y)
+        if super().is_wall_ahead(self.mapdata, self.position, self.direction):
+            pos_x, pos_y = super().last_position(self.position, self.direction)
 
         # jump to the other side
-        pos_x, pos_y = self.check_borders(pos_x, pos_y)
+        pos_x, pos_y = super().jump_border(self.mapdata, self.position)
 
         return (pos_x, pos_y)
 
-    def set_score(self, pos_x: int, pos_y: int):
-        """ Returns the score value of the given coordinate
 
-        @args:
-            (pos_x, pos_y) [int, int] - the player current position
+    def set_score(self):
+        """ Returns the score value on the player current position
+
         @return:
             score [int] - the value of the reward on the given coordinates
         """
+
+        pos_x = self.position[0]
+        pos_y = self.position[1]
 
         # check for points
         if (pos_x, pos_y) in self.mapdata.collectables.points:
