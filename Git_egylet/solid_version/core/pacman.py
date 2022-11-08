6e780c4d1f62e703f2bb93e8f310ce9f0e553147
benceuveges <103946807+benceuveges@users.noreply.github.com>
2022-11-08 02:12:19 +0100

    Add Pacman class
    Added Pacman class which handles the movement of Pacman and contributes to visualization

--- Git_egylet/solid_version/core/pacman.py
+++ Git_egylet/solid_version/core/pacman.py
@@ -7,3 +7,22 @@ from bosch_ASDIIE_Git_egylet.solid_version.core.key_event import KeyEvent
 from bosch_ASDIIE_Git_egylet.solid_version.core.map import Coordinates, MapSize
 from bosch_ASDIIE_Git_egylet.solid_version.core.visualizable import Visualizable
 from bosch_ASDIIE_Git_egylet.solid_version.core.moving_transformation import MovingTransformation
+
+class Pacman(GameElement, Visualizable):
+    """
+    A game element and visualizable class, for handling Pacman movement mainly,
+    but visualizing is also represented here
+    """
+    def __init__(self,
+                 body: Deque[Coordinates] = None,
+                 starting_direction: KeyEvent = KeyEvent.RIGHT,
+                 map_size: MapSize = None):
+        if body is None:
+            self.body_parts = deque([
+                Coordinates(0, 1)
+            ])
+        else:
+            self.body_parts = body
+        if map_size is None:
+            map_size = MapSize(10, 10)
+        self.moving_transformation = MovingTransformation(starting_direction, map_size)
