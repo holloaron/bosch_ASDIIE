adfcb2ff03c75ea3d61a7b52172723ccc8163f1a
4doschris <fordos.krisztian@gmail.com>
2022-11-08 21:32:10 +0100

    Import modules for Wallgenerator class

---
+++ Git_egylet/solid_version/core/wallgenerator.py
@@ -0,0 +1,6 @@
+from collections import deque
+from typing import Deque
+
+from bosch_ASDIIE_Git_egylet.solid_version.core.canvas import Canvas
+from bosch_ASDIIE_Git_egylet.solid_version.core.map import Coordinates, MapSize
+from bosch_ASDIIE_Git_egylet.solid_version.core.visualizable import Visualizable
