d384bfd840fa4cb06d53bf807ff6e5d7cc38b348
farkas-peter <peterfarkas@edu.bme.hu>
2022-11-09 09:01:48 +0100

    Check collision function is removed to Map class and agent position is changed to be an array

--- Namco/map.py
+++ Namco/map.py
@@ -12,19 +12,42 @@ class Map:
         # Filling up the map with dots
         self.map[self.map == ' '] = 'x'
 
-    def update_map(self, pacman_x: int, pacman_y: int, done: bool) -> np.ndarray:
+    def update_map(self, agent_pos: [int, int], done: bool) -> np.ndarray:
         """
         Updates the map based on PacMan's position
-        :param pacman_x: PacMan's x position (int)
-        :param pacman_y: PacMan's y position (int)
-        :param done: Game over flag (bool)
+        :param agent_pos: x and y coordinate of the agent (int, int)
+        :param done: game over flag (bool)
         :return: updated map (np.ndarray)
         """
+        x = agent_pos[0]
+        y = agent_pos[1]
+
         # PacMan's previous position must be emptied
         self.map[self.map == '0'] = ' '
         # PacMan should be placed into its current position
-        self.map[pacman_x][pacman_y] = '0'
+        self.map[x][y] = '0'
         # Marking place of death on map
         if done:
             self.map[self.map == '0'] = 'X'
+
         return self.map
+
+    def check_collision(self, agent_pos: [int, int]) -> [bool, bool]:
+        """
+        Checks whether Pacman interacted with any objects on the map
+        :return: wall collision flag (bool), dot collision flag (bool)
+        """
+        dot_collision = False
+        wall_collision = False
+
+        x = agent_pos[0]
+        y = agent_pos[1]
+
+        # Checking whether PacMan stepped on a wall
+        if self.map[x][y] == '-' or self.map[x][y] == '|':
+            wall_collision = True
+        # Checking whether PacMan collected an eatable dot
+        elif self.map[x][y] == 'x':
+            dot_collision = True
+
+        return dot_collision, wall_collision
