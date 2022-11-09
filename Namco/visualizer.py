92b7ad3a8f488320c0035da45dec55a36526b602
farkas-peter <peterfarkas@edu.bme.hu>
2022-11-09 08:49:57 +0100

    staticmethod is added to the functions where it is necessary

--- Namco/visualizer.py
+++ Namco/visualizer.py
@@ -4,7 +4,8 @@ import numpy as np
 
 
 class Visualizer:
-    def clear_console(self):
+    @staticmethod
+    def clear_console():
         """
         Clearing the console
         :return:
@@ -12,7 +13,8 @@ class Visualizer:
         # Note: Cannot clear the console in PyCharm, only in terminal
         os.system('cls' if os.name == 'nt' else 'clear')
 
-    def show_map(self, observation: np.ndarray):
+    @staticmethod
+    def show_map(observation: np.ndarray):
         """
         Visualizing the current state of the game to the player
         :param observation: Array containing the map (np.ndarray)
@@ -21,7 +23,8 @@ class Visualizer:
         for line in observation:
             print(*line, sep='  ')
 
-    def show_score(self, curr_score: int):
+    @staticmethod
+    def show_score(curr_score: int):
         """
         Informing the user about the current score
         :param curr_score: Current score of the player (int)
@@ -29,7 +32,8 @@ class Visualizer:
         """
         print("\nCurrent score: ", curr_score)
 
-    def game_over(self, done: bool):
+    @staticmethod
+    def game_over(done: bool):
         """
         Informing the user that the game is over
         :param done: Game over flag
