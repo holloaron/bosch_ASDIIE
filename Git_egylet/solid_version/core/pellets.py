64882b8d172cabc278a4c04c6d7e48c22a2e4bfc
gujgidani <gujgiczerd@edu.bme.hu>
2022-11-10 23:49:10 +0100

    Add make pellets function
    Added make pellets function which creates pellets randomly around the map. Yet to be implemented fully.

--- Git_egylet/solid_version/core/pellets.py
+++ Git_egylet/solid_version/core/pellets.py
@@ -1,31 +1,40 @@
-from collections import deque
-from typing import Deque
+from typing import List
+import numpy as np
 
 from bosch_ASDIIE.Git_egylet.solid_version.core.canvas import Canvas
 from bosch_ASDIIE.Git_egylet.solid_version.core.game_element import GameElement
 from bosch_ASDIIE.Git_egylet.solid_version.core.key_event import KeyEvent
 from bosch_ASDIIE.Git_egylet.solid_version.core.map import Coordinates, MapSize
 from bosch_ASDIIE.Git_egylet.solid_version.core.visualizable import Visualizable
+from bosch_ASDIIE.Git_egylet.solid_version.core.screen import Screen
 
 
-class Pellets(GameElement,Visualizable):
+class Pellets(GameElement, Visualizable):
     """
     A visualizable class, for handling Pellets
     """
     def __init__(self,
-                 body: Deque[Coordinates] = None,
-                 map_size: MapSize = None):
-        if body is None:
-            self.body_parts = deque([
-                Coordinates(2, 2), Coordinates(3, 3), Coordinates(4, 4)
-            ])
-        else:
-            self.body_parts = body
+                 map_size: MapSize = None,
+                 number_pellets: int = 10,
+                 ):
         if map_size is None:
             map_size = MapSize(10, 10)
 
+        self.positions = self.make_pellets(number=number_pellets, map_size=map_size)
+
+    def make_pellets(self, number: int, map_size: MapSize, screen: Screen) -> List[Coordinates]:
+        pellet_positions = []
+        for _ in range(number):
+            position = Coordinates(np.random.randint(map_size[0]), np.random.randint(map_size[1]))
+            #TODO: if there's another object in the map, generate a new position
+            #while screen(position) != " ":
+                #position = Coordinates(np.random.randint(map_size[0]), np.random.randint(map_size[1]))
+            pellet_positions.append(position)
+
+        return pellet_positions
+
     def draw(self, canvas: Canvas):
-        canvas.draw_dots(self.body_parts)
+        canvas.draw_dots(self.positions)
 
     def take_action(self, key_event: KeyEvent):
         pass
