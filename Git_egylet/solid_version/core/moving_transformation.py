dd27aec3e6b401ed46c62eb37e95cc4cbda400bd
gujgidani <gujgiczerd@edu.bme.hu>
2022-11-10 18:21:37 +0100

    Fix module importation in MovingTransformation

--- Git_egylet/solid_version/core/moving_transformation.py
+++ Git_egylet/solid_version/core/moving_transformation.py
@@ -1,5 +1,6 @@
-from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.key_event import KeyEvent
-from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.map import Coordinates, MapSize
+from bosch_ASDIIE.Git_egylet.solid_version.core.key_event import KeyEvent
+from bosch_ASDIIE.Git_egylet.solid_version.core.map import Coordinates, MapSize
+
 
 class MovingTransformation:
     """
