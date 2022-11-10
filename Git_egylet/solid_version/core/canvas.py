bcf0caf9cd77fc0861abd5b9994cc2cb6bd09c62
gujgidani <gujgiczerd@edu.bme.hu>
2022-11-10 18:19:34 +0100

    Fix module importation in Canvas class

--- Git_egylet/solid_version/core/canvas.py
+++ Git_egylet/solid_version/core/canvas.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import List
 
-from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.map import Coordinates
+from bosch_ASDIIE.Git_egylet.solid_version.core.map import Coordinates
 
 
 class Canvas(ABC):
