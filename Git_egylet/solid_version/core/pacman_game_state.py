5ff48e1b761f665e9ac0d0258e17a9586f52b6cd
gujgidani <gujgiczerd@edu.bme.hu>
2022-11-10 18:22:05 +0100

    Fix module importation in PacmanGameState class

--- Git_egylet/solid_version/core/pacman_game_state.py
+++ Git_egylet/solid_version/core/pacman_game_state.py
@@ -1,7 +1,7 @@
 from typing import List
 
-from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.game_element import GameElement
-from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.key_event import KeyEvent
+from bosch_ASDIIE.Git_egylet.solid_version.core.game_element import GameElement
+from bosch_ASDIIE.Git_egylet.solid_version.core.key_event import KeyEvent
 
 
 class PacmanGameState:
