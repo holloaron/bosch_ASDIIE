d4ec395a577df0e6f29981d18da4441065e8b50b
gujgidani <gujgiczerd@edu.bme.hu>
2022-11-11 00:16:15 +0100

    Import needed modules for Terminate class

---
+++ Git_egylet/solid_version/core/terminate.py
@@ -0,0 +1,4 @@
+from bosch_ASDIIE.Git_egylet.solid_version.core.game_element import GameElement
+from bosch_ASDIIE.Git_egylet.solid_version.core.key_event import KeyEvent
+from bosch_ASDIIE.Git_egylet.solid_version.core.pacman import Pacman
+from bosch_ASDIIE.Git_egylet.solid_version.core.wallgenerator import Wallgenerator
