9c1589728c0ed9ea8d56b28557a20d439c2a2b5f
gujgidani <gujgiczerd@edu.bme.hu>
2022-11-10 18:23:07 +0100

    Fix module importation in Pellets class

--- Git_egylet/solid_version/core/pellets.py
+++ Git_egylet/solid_version/core/pellets.py
@@ -1,11 +1,12 @@
 from collections import deque
 from typing import Deque
 
-from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.canvas import Canvas
-from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.game_element import GameElement
-from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.key_event import KeyEvent
-from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.map import Coordinates, MapSize
-from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.visualizable import Visualizable
+from bosch_ASDIIE.Git_egylet.solid_version.core.canvas import Canvas
+from bosch_ASDIIE.Git_egylet.solid_version.core.game_element import GameElement
+from bosch_ASDIIE.Git_egylet.solid_version.core.key_event import KeyEvent
+from bosch_ASDIIE.Git_egylet.solid_version.core.map import Coordinates, MapSize
+from bosch_ASDIIE.Git_egylet.solid_version.core.visualizable import Visualizable
+
 
 class Pellets(GameElement,Visualizable):
     """
