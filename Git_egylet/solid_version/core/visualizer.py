88b9a1dff5341561e6051876e2cd888048acf254
gujgidani <gujgiczerd@edu.bme.hu>
2022-11-10 18:24:50 +0100

    Fix module importation in Visualizer class

--- Git_egylet/solid_version/core/visualizer.py
+++ Git_egylet/solid_version/core/visualizer.py
@@ -1,7 +1,7 @@
 from typing import List
 
-from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.canvas import Canvas
-from bosch_ASDIIE_Git_egylet.Git_egylet.solid_version.core.visualizable import Visualizable
+from bosch_ASDIIE.Git_egylet.solid_version.core.canvas import Canvas
+from bosch_ASDIIE.Git_egylet.solid_version.core.visualizable import Visualizable
 
 
 class Visualizer:
