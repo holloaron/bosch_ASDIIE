865b86a5db54a85ac5a1670b2b5ade1bbf420a96
markbozsoki <83731022+markbozsoki@users.noreply.github.com>
2022-11-11 10:16:11 +0100

    CHG: PacMan.py - threading fixed

--- SegmentFault/PacMan.py
+++ SegmentFault/PacMan.py
@@ -33,66 +33,68 @@ from source.ui.GrayScaleVisualizer import GrayScaleVisualizer
 class PacMan:
     def __init__(self):
         self.game_over = False
+        self.user_input = Inputs.Nothing
 
         self.terminal = Terminal()
         self.config = Config()
-        #self.GAMEMODE, self.TIMEOUTLIMIT, self.MAP, self.GAMESPEED = Config.Getsettings()
         self.GAMEMODE, self.TIMEOUTLIMIT, self.MAP, self.GAMESPEED = self.config.Getsettings()
         self.timer = TimeCounter(self.TIMEOUTLIMIT)
 
-        # init empty mapdata
         self.mapdata = MapData()
 
         self.show_menu()
 
 
     def show_menu(self):
-        menuinput = Inputs.Nothing
+        """ 
+        """
 
-        while menuinput != "exit":
+        while self.user_input != "exit":
 
+            self.game_over = False
+            self.user_input = Inputs.Nothing
             self.terminal.show_menu("Main Menu", self.terminal.main_menuitems)
-            menuinput = self.terminal.get_menu_input("Choose menu option!", self.terminal.main_menuitems)
+            self.user_input = self.terminal.get_menu_input("Choose menu option!", self.terminal.main_menuitems)
 
             # START GAME
-            if menuinput == "start game":
+            if self.user_input == "start game":
                 self.start_game_session()
-                menuinput = Inputs.Nothing
+                self.user_input = Inputs.Nothing
 
             # GAMEMODE SETTINGS
-            if menuinput == "game mode":
-                while menuinput != "back":
+            if self.user_input == "game mode":
+                while self.user_input != "back":
                     self.terminal.show_menu("Change Gamemode", self.terminal.gamemode_menuitems)
-                    menuinput = self.terminal.get_menu_input("Choose gamemode option!", self.terminal.gamemode_menuitems)
+                    self.user_input = self.terminal.get_menu_input("Choose gamemode option!", self.terminal.gamemode_menuitems)
                     
-                    if menuinput == "standard":
+                    if self.user_input == "standard":
                         self.GAMEMODE = "standard"
 
-                    if menuinput == "sandbox":
+                    if self.user_input == "sandbox":
                         self.GAMEMODE = "sandbox"
 
             # MAP SETTINGS
-            if menuinput == "change map":
-                while menuinput != "back":
+            if self.user_input == "change map":
+                while self.user_input != "back":
                     self.terminal.show_menu("Maps", self.terminal.changemap_menuitems)
-                    menuinput = self.terminal.get_menu_input("Choose map option!", self.terminal.changemap_menuitems)
+                    self.user_input = self.terminal.get_menu_input("Choose map option!", self.terminal.changemap_menuitems)
                     
-                    if menuinput == "Basic":
+                    if self.user_input == "Basic":
                         self.MAP = "Basic.mapdat"
                     
-                    if menuinput == "Bosch ASDIIE":
+                    if self.user_input == "Bosch ASDIIE":
                         self.MAP = "Bosch ASDIIE.mapdat"
 
-                    if menuinput == "Clyde's Maze":
+                    if self.user_input == "Clyde's Maze":
                         self.MAP = "Clyde's Maze.mapdat"
 
-                    if menuinput == "Double Trouble":
+                    if self.user_input == "Double Trouble":
                         self.MAP = "Double Trouble.mapdat"
 
-                    if menuinput == "Horizontal":
+                    if self.user_input == "Horizontal":
                         self.MAP = "Horizontal.mapdat"
 
-                    if menuinput == "Vertical":
+                    if self.user_input == "Vertical":
                         self.MAP = "Vertical.mapdat"
 
                     self.GAMEMODE = "standard"
@@ -103,8 +105,8 @@ class PacMan:
 
 
     def start_game_session(self):
-
-        #TODO: init mapdata based on config
+        """ Configure session based on the settings and starts the game
+        """
 
         # init mapdata
         if self.GAMEMODE == "standard":
@@ -116,13 +118,10 @@ class PacMan:
             map_generator.generate_mapdata(map_width=25, map_height=25)
             self.mapdata = map_generator.fill_mapdata()
             
-        self.timer.StartStopperAdvanced()
-        #self.timer.reset()
-        #self.timer.run()
-
         # start game
-        player_input_thread = threading.Thread(target=self.get_user_input())
-        game_state = threading.Thread(target=self.render_game_state())
+        self.timer.StartStopperAdvanced()
+        player_input_thread = threading.Thread(target=self.get_user_input)
+        game_state = threading.Thread(target=self.calculate_game_state)
 
         player_input_thread.start()
         game_state.start()
@@ -131,13 +130,26 @@ class PacMan:
         player_input_thread.join()
         game_state.join()
 
-        #self.timer.stop()
-
 
     def get_user_input(self):
-        player_input = Inputs.Nothing
+        """ Resives the user input during the game session
+        """
+        while not self.game_over or not self.timer.is_timeout:
 
-        #TODO: init Ghosts based on config
+            self.terminal.clear()
+            self.terminal.show_title()
+
+            self.user_input = Inputs.Nothing
+            self.user_input = self.terminal.get_gameplay_input("Set new direction!")
+
+            if self.user_input == Inputs.Exit:
+                self.game_over = True
+                break
+
+
+    def calculate_game_state(self):
+        """ Implements the gameplay logic
+        """
 
         # init Player (PacMan)
         player = Player(self.mapdata, self.mapdata.Player, Direction.Down)
@@ -147,68 +159,71 @@ class PacMan:
             for safe_direction in Direction.__members__.values():
                 if not player.is_wall_infront(self.mapdata, player.position, safe_direction):
                     player.direction = safe_direction
+                    break
+
+        #TODO: init Ghosts based on config
 
         #TODO: init enemies and collectables
 
         while not self.game_over or not self.timer.is_timeout:
-            self.terminal.clear()
-            self.terminal.show_title()
 
-            player_input = self.terminal.get_gameplay_input("Set new direction!")
-
-            if player_input == Inputs.Restart:
-                #TODO: implement game restart logic
-                self.restart_game_session()
-                break
-
-            if player_input == Inputs.Exit:
-                self.game_over = True
-                break
-
-            # direction changing
-            if player_input == Inputs.SetDirection_Up:
+            # player direction changing
+            if self.user_input == Inputs.SetDirection_Up:
                 player.next_direction = Direction.Up
 
-            if player_input == Inputs.SetDirection_Right:
+            if self.user_input == Inputs.SetDirection_Right:
                 player.next_direction = Direction.Right
             
-            if player_input == Inputs.SetDirection_Down:
+            if self.user_input == Inputs.SetDirection_Down:
                 player.next_direction = Direction.Down
 
-            if player_input == Inputs.SetDirection_Left:
+            if self.user_input == Inputs.SetDirection_Left:
                 player.next_direction = Direction.Left
 
             # refresh player position
             if self.GAMEMODE == "sandbox":
                 # game over in sandbox mode if player hits the wall
                 last_player_position = player.position
-                if player.get_next_position() == last_player_position:
+                player.position = player.get_next_position()
+                if player.position == last_player_position:
                     # if the player not moved, than reached a wall -> game over
                     break
             else:
                 player.position = player.get_next_position()
 
-            # game session timeout
-            if self.timer.is_timeout():
+            # refresh mapdata info
+            self.mapdata.Player = player.position
+
+            # special command handling
+            if self.user_input == Inputs.Restart:
+                #TODO: implement game restart logic
+                self.restart_game_session()
                 break
 
+            if self.user_input == Inputs.Exit:
+                self.game_over = True
+                break
+
+            self.render_game_state()
+            time.sleep(self.GAMESPEED)
+        
         game_time = 2#self.timer.seconds_passed()
         self.terminal.show_gameplayresult(game_time, player.score)
-
-
-    def render_game_state(self):
-        visualiser = GrayScaleVisualizer(size_ratio=10)
-
-        while not self.game_over or not self.timer.is_timeout:
-            visualiser.refresh_game_state(self.mapdata)
-            visualiser.render_game_state(self.mapdata.size)
-
-            time.sleep(self.GAMESPEED)
 
 
     def restart_game_session(self):
         #TODO: implement session restart
         pass
+
+
+    def render_game_state(self):
+        """
+        """
+        visualiser = GrayScaleVisualizer(size_ratio=10)
+
+        visualiser.refresh_game_state(self.mapdata)
+        visualiser.render_game_state(self.mapdata.size)
+
 
 # program entry point
 if __name__ == "__main__":
