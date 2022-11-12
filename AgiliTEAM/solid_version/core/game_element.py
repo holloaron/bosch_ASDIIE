9edcb51e551763948b68e803c363ca2c46977bf8
BPelenczei <81440132+BPelenczei@users.noreply.github.com>
2022-11-12 22:35:15 +0100

    ADD empty docstring, typehint to game_element.py

--- AgiliTEAM/solid_version/core/game_element.py
+++ AgiliTEAM/solid_version/core/game_element.py
@@ -6,8 +6,17 @@ from bosch_ASDIIE.AgiliTEAM.solid_version.core.key_event import KeyEvent
 class GameElement(ABC):
     @abstractmethod
     def take_action(self, key_event: KeyEvent):
+        """
+
+        :param key_event:
+        :return:
+        """
         pass
 
     @abstractmethod
     def tick(self) -> bool:
+        """
+
+        :return:
+        """
         pass
