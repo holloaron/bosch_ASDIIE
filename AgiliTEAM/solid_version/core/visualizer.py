0a3b1b239757b6de960191952cbce78a55dbb491
BPelenczei <81440132+BPelenczei@users.noreply.github.com>
2022-11-12 22:32:37 +0100

    ADD empty docstring, typehint to visualizer.py

--- AgiliTEAM/solid_version/core/visualizer.py
+++ AgiliTEAM/solid_version/core/visualizer.py
@@ -6,11 +6,20 @@ from bosch_ASDIIE.AgiliTEAM.solid_version.core.visualizable import Visualizable
 class Visualizer:
     def __init__(self,
                  visualizable_objects: List[Visualizable],
-                 canvas: Canvas):
+                 canvas: Canvas) -> None:
+        """
+
+        :param visualizable_objects:
+        :param canvas:
+        """
         self.visualizable_objects = visualizable_objects
         self.canvas = canvas
 
-    def render(self):
+    def render(self) -> None:
+        """
+
+        :return: None
+        """
         self.canvas.clear()
         for obj in self.visualizable_objects:
             obj.draw(self.canvas)
