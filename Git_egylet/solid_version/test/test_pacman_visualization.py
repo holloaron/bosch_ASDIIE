26aba7c10dfc0208ca450832dc3e75bec44de6e6
4doschris <fordos.krisztian@gmail.com>
2022-11-12 11:12:02 +0100

    Add SpyCanvas class
    Created SpyCanvas class and its constructor.

--- Git_egylet/solid_version/test/test_pacman_visualization.py
+++ Git_egylet/solid_version/test/test_pacman_visualization.py
@@ -6,3 +6,8 @@ from bosch_ASDIIE.Git_egylet.solid_version.core.key_event import KeyEvent
 from bosch_ASDIIE.Git_egylet.solid_version.core.map import Coordinates, MapSize
 from bosch_ASDIIE.Git_egylet.solid_version.core.pacman import Pacman
 from bosch_ASDIIE.Git_egylet.solid_version.gui.console_canvas import ConsoleCanvas
+
+class SpyCanvas(Canvas):
+    def __init__(self, canvas: Canvas):
+        self.canvas = canvas
+        self._dots_on_canvas = []
