7eddf762a0d1655abd897f8476683fb1a1c8d2e8
BPelenczei <81440132+BPelenczei@users.noreply.github.com>
2022-11-12 22:35:46 +0100

    ADD empty docstring, typehint to canvas.py

--- AgiliTEAM/solid_version/core/canvas.py
+++ AgiliTEAM/solid_version/core/canvas.py
@@ -7,20 +7,42 @@ from bosch_ASDIIE.AgiliTEAM.solid_version.core.map import Coordinates
 class Canvas(ABC):
     @abstractmethod
     def draw_dots(self, coordinates: List[Coordinates], obj_type: str = None):
+        """
+
+        :param coordinates:
+        :param obj_type:
+        :return:
+        """
         pass
 
     @abstractmethod
     def render(self):
+        """
+
+        :return:
+        """
         pass
 
     @abstractmethod
     def clear(self):
+        """
+
+        :return:
+        """
         pass
 
     @abstractmethod
     def get_height(self):
+        """
+
+        :return:
+        """
         pass
 
     @abstractmethod
     def get_width(self):
+        """
+
+        :return:
+        """
         pass
