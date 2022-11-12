31d6fd88c52fc1613f334f2fcd6adbafc27433b4
4doschris <fordos.krisztian@gmail.com>
2022-11-12 10:59:28 +0100

    Import modules for visualization test

--- Git_egylet/solid_version/test/test_pacman_visualization.py
+++ Git_egylet/solid_version/test/test_pacman_visualization.py
@@ -1,2 +1,8 @@
 from collections import deque
 from typing import List
+
+from bosch_ASDIIE.Git_egylet.solid_version.core.canvas import Canvas
+from bosch_ASDIIE.Git_egylet.solid_version.core.key_event import KeyEvent
+from bosch_ASDIIE.Git_egylet.solid_version.core.map import Coordinates, MapSize
+from bosch_ASDIIE.Git_egylet.solid_version.core.pacman import Pacman
+from bosch_ASDIIE.Git_egylet.solid_version.gui.console_canvas import ConsoleCanvas
