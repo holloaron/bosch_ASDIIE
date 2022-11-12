b4eef4343a0df526984df13e00b9fefa059c8d35
BPelenczei <81440132+BPelenczei@users.noreply.github.com>
2022-11-12 22:32:51 +0100

    ADD empty docstring, typehint to visualizable.py

--- AgiliTEAM/solid_version/core/visualizable.py
+++ AgiliTEAM/solid_version/core/visualizable.py
@@ -5,5 +5,10 @@ from bosch_ASDIIE.AgiliTEAM.solid_version.core.canvas import Canvas
 
 class Visualizable(ABC):
     @abstractmethod
-    def draw(self, canvas: Canvas):
+    def draw(self, canvas: Canvas) -> None:
+        """
+
+        :param canvas:
+        :return: None
+        """
         pass
