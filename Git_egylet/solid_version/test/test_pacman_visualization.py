0fbc1370f43ea8e8701be0a649b06fcc580b4d61
4doschris <fordos.krisztian@gmail.com>
2022-11-12 12:02:21 +0100

    Add test for movement
    Added movement test to visualization test code.

--- Git_egylet/solid_version/test/test_pacman_visualization.py
+++ Git_egylet/solid_version/test/test_pacman_visualization.py
@@ -11,3 +11,38 @@ class SpyCanvas(Canvas):
     def __init__(self, canvas: Canvas):
         self.canvas = canvas
         self._dots_on_canvas = []
+
+    @property
+    def dots_on_canvas(self):
+        return self._dots_on_canvas
+
+    def draw_dots(self, coordinates: List[Coordinates], object_type: str):
+        self._dots_on_canvas.extend(coordinates)
+        self.canvas.draw_dots(coordinates)
+
+    def clear(self):
+        self._dots_on_canvas = []
+
+    def render(self):
+        pass
+
+def testCanvas_whenPacmanMoving_itAppearsOnNewCoordinates():
+    canvas = ConsoleCanvas(10, 10)
+    spy_canvas = SpyCanvas(canvas)
+    pacman = Pacman(
+        deque([Coordinates(0, 1), Coordinates(0, 2), Coordinates(0, 3)]),
+        starting_direction=KeyEvent.RIGHT
+    )
+    pacman.draw(spy_canvas)
+    dots_before_moving = spy_canvas.dots_on_canvas
+    spy_canvas.clear()
+    pacman.tick()
+    pacman.draw(spy_canvas)
+    dots_after_moving = spy_canvas.dots_on_canvas
+    assert Coordinates(0, 1) in dots_before_moving and \
+           Coordinates(0, 1) not in dots_after_moving, \
+        "PacMan should not be on the canvas after moving."
+    assert Coordinates(0, 4) in dots_before_moving and \
+           Coordinates(0, 4) not in dots_after_moving, \
+        "PacMan should be moved on canvas after moving."
+    
