740b31467bfe207b22ee779931d201946b093321
4doschris <fordos.krisztian@gmail.com>
2022-11-12 14:55:01 +0100

    Add OppositeKeyEvent test function
    Added OppositeKeyEvent test function to visualization test code.

--- Git_egylet/solid_version/test/test_pacman_visualization.py
+++ Git_egylet/solid_version/test/test_pacman_visualization.py
@@ -45,3 +45,25 @@ def testCanvas_whenPacmanMoving_itAppearsOnNewCoordinates():
     assert Coordinates(0, 4) in dots_before_moving and \
            Coordinates(0, 4) not in dots_after_moving, \
         "PacMan should be moved on canvas after moving."
+
+def testCanvas_whenPacmanMoving_thenOppositeKeyEventDoesNotDoAnything():
+    canvas = ConsoleCanvas(10, 10)
+    spy_canvas = SpyCanvas(canvas)
+    pacman = Pacman(
+        deque([Coordinates(0, 1), Coordinates(0, 2), Coordinates(0, 3)]),
+        starting_direction=KeyEvent.RIGHT
+    )
+    pacman.draw(spy_canvas)
+    dots_before_moving = spy_canvas.dots_on_canvas
+    spy_canvas.clear()
+    pacman.take_action(KeyEvent.LEFT)
+    pacman.tick()
+    pacman.draw(spy_canvas)
+    dots_after_moving = spy_canvas.dots_on_canvas
+    assert Coordinates(0, 1) in dots_before_moving and \
+           Coordinates(0, 1) not in dots_after_moving, \
+        "Pacman should not be displayed on the previous coordinates after moving."
+    assert Coordinates(0, 4) not in dots_before_moving and \
+           Coordinates(0, 4) in dots_after_moving, \
+        "Pacman should be moved to forward the original direction " \
+        "after pacman got opposite KeyEvent."
