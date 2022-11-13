5ed2a338267399c4fef1c89986be8cd5d42bdfb8
ArthurMorgen <nyestepallaszlo@gmail.com>
2022-11-13 16:09:47 +0100

    FIXING Unit tests

---
+++ AgiliTEAM/solid_version/test/pellet_test/pellet_test.py
@@ -0,0 +1,15 @@
+from solid_version.core.map import Coordinates
+from solid_version.core.map import MapSize
+from solid_version.core.pellets import Pellets
+import unittest
+
+def test_NumberOfThePellets(self):
+    pellets = Pellets(map_size=MapSize(2, 2), num_pellets=3, known_pos=[[Coordinates(0, 0)]])
+    self.assertFalse(len(pellets.known_pos) == 3,
+                     "Error during pellet generation, value mismatch")
+
+
+def test_PelletGeneration(self):
+    pellets = Pellets(map_size=MapSize(2, 2), num_pellets=3, known_pos=[[Coordinates(0, 0)]])
+    self.assertTrue(pellets.pos.count([Coordinates(0, 0)]) == 0,
+                    "Error during pellet generation, invalid pellet position")
