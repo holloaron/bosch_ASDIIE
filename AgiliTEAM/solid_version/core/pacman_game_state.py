b9744b1b614006ec9fb68c399e19b228cfff9acb
BPelenczei <81440132+BPelenczei@users.noreply.github.com>
2022-11-13 16:38:13 +0100

    ADD import of walls

--- AgiliTEAM/solid_version/core/pacman_game_state.py
+++ AgiliTEAM/solid_version/core/pacman_game_state.py
@@ -2,9 +2,9 @@ from typing import List
 
 from bosch_ASDIIE.AgiliTEAM.solid_version.core.interface.game_element import GameElement
 from bosch_ASDIIE.AgiliTEAM.solid_version.core.key_interaction.key_event import KeyEvent
-
 from bosch_ASDIIE.AgiliTEAM.solid_version.core.game_element.ghosts import Ghosts
 from bosch_ASDIIE.AgiliTEAM.solid_version.core.game_element.pacman import Pacman
+from bosch_ASDIIE.AgiliTEAM.solid_version.core.game_element.walls import Walls
 
 
 class PacmanGameState:
