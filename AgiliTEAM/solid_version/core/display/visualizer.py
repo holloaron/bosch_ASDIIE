88b528fd69bac85e8c384ce2ce0b62a03018439f
BPelenczei <81440132+BPelenczei@users.noreply.github.com>
2022-11-13 16:43:30 +0100

    ADD empty docstrings

--- AgiliTEAM/solid_version/core/display/visualizer.py
+++ AgiliTEAM/solid_version/core/display/visualizer.py
@@ -7,10 +7,19 @@ class Visualizer:
     def __init__(self,
                  visualizable_objects: List[Visualizable],
                  canvas: Canvas):
+        """
+
+        :param visualizable_objects:
+        :param canvas:
+        """
         self.visualizable_objects = visualizable_objects
         self.canvas = canvas
 
-    def render(self):
+    def render(self) -> None:
+        """
+
+        :return: None
+        """
         self.canvas.clear()
         for obj in self.visualizable_objects:
             obj.draw(self.canvas)
