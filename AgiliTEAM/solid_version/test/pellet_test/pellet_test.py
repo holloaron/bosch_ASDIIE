e65289df05c11ee5b96dd6806ee30d23df0572d9
BPelenczei <81440132+BPelenczei@users.noreply.github.com>
2022-11-13 18:05:02 +0100

    FIX PEP8 format

--- AgiliTEAM/solid_version/test/pellet_test/pellet_test.py
+++ AgiliTEAM/solid_version/test/pellet_test/pellet_test.py
@@ -2,13 +2,14 @@ from bosch_ASDIIE.AgiliTEAM.solid_version.core.misc.map import MapSize, Coordina
 from bosch_ASDIIE.AgiliTEAM.solid_version.core.game_element.pellets import Pellets
 import unittest
 
+
 class PelletTest(unittest.TestCase):
     def test_NumberOfThePellets(self):
         pellets = Pellets(map_size=MapSize(2, 2), num_pellets=3, known_pos=[[Coordinates(0, 0)]])
         self.assertFalse(len(pellets.known_pos) == 3,
                          "Error during pellet generation, value mismatch")
 
-
     def test_PelletGeneration(self):
         pellets = Pellets(map_size=MapSize(2, 2), num_pellets=3, known_pos=[[Coordinates(0, 0)]])
-        self.assertTrue(pellets.pos.count([Coordinates(0, 0)]) == 0, "Error during pellet generation, invalid pellet position")
+        self.assertTrue(pellets.pos.count([Coordinates(0, 0)]) == 0, "Error during pellet generation, invalid pellet"
+                                                                     "position")
