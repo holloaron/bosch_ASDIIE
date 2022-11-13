5123729b6ca0401f072f9251686211e2416cbdeb
BPelenczei <81440132+BPelenczei@users.noreply.github.com>
2022-11-13 16:46:22 +0100

    ADD usage of pos generator

--- AgiliTEAM/solid_version/core/game_element/pellets.py
+++ AgiliTEAM/solid_version/core/game_element/pellets.py
@@ -29,20 +29,12 @@ class Pellets(GameElement, Visualizable):
 
         pos_generator = PositionGenerator(map_size, self.known_pos)
 
-        self.pos = self.generate_pos(num_of_pos=num_pellets, map_size=map_size)
+        self.pos, self.known_pos = pos_generator.generate_pos(num_of_pos=num_pellets)
 
-    def generate_pos(self, num_of_pos: int, map_size: MapSize) -> List[Coordinates]:
-        pos_list = []
-        for _ in range(num_of_pos):
-            pos = Coordinates(np.random.randint(map_size[0]), np.random.randint(map_size[1]))
-            while pos in self.known_pos:
-                pos = Coordinates(np.random.randint(map_size[0]), np.random.randint(map_size[1]))
-            pos_list.append(pos)
-            self.known_pos.append(pos)
+    def take_action(self, key_event: KeyEvent) -> None:
+        """
 
-        return pos_list
-
-    def take_action(self, key_event: KeyEvent):
+        """
         pass
 
     def tick(self) -> bool:
