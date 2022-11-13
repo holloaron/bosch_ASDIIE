44fc7d33ec6bc5c83d9cb42b5a2bda439d43acbc
BPelenczei <81440132+BPelenczei@users.noreply.github.com>
2022-11-13 16:42:07 +0100

    CHANGE implementation of __call__ function in MovingTransformation class

--- AgiliTEAM/solid_version/core/key_interaction/move.py
+++ AgiliTEAM/solid_version/core/key_interaction/move.py
@@ -21,15 +21,20 @@ class MovingTransformation:
         """
         if self.direction == KeyEvent.UP:
             new_row = coordinates.row if coordinates.row - 1 < 0 else coordinates.row - 1
-            return Coordinates(new_row, coordinates.col)
+            new_pos = Coordinates(new_row, coordinates.col)
         elif self.direction == KeyEvent.LEFT:
             new_col = coordinates.col if coordinates.col - 1 < 0 else coordinates.col - 1
-            return Coordinates(coordinates.row, new_col)
+            new_pos = Coordinates(coordinates.row, new_col)
         elif self.direction == KeyEvent.DOWN:
             new_row = coordinates.row if coordinates.row + 1 == self.map_size.row_num else coordinates.row + 1
-            return Coordinates(new_row, coordinates.col)
+            new_pos = Coordinates(new_row, coordinates.col)
         elif self.direction == KeyEvent.RIGHT:
             new_col = coordinates.col if coordinates.col + 1 == self.map_size.col_num else coordinates.col + 1
-            return Coordinates(coordinates.row, new_col)
+            new_pos = Coordinates(coordinates.row, new_col)
         else:
             raise ValueError(f"There is no moving forward {self.direction} direction.")
+
+        if new_pos not in self.prohibited_pos:
+            return new_pos
+        else:
+            return coordinates
