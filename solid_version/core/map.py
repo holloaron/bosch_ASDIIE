04bb44d49273ae2e9b09fc884829a66dcc14dd94
Márk Bozsóki <83731022+markbozsoki@users.noreply.github.com>
2022-11-13 21:06:45 +0100

    Delete sample solid version dir

--- solid_version/core/map.py
+++
@@ -1,4 +0,0 @@
-from collections import namedtuple
-
-Coordinates = namedtuple("Coordinates", ("row", "col"))
-MapSize = namedtuple("MapSize", ("row_num", "col_num"))
--- solid_version/core/visualizable.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-from bosch_ASDIIE.solid_version.core.canvas import Canvas
-
-
-class Visualizable(ABC):
-    @abstractmethod
-    def draw(self, canvas: Canvas):
-        pass
