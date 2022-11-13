287e5f09933a63e2bec9163a19e21033f39f1504
BPelenczei <81440132+BPelenczei@users.noreply.github.com>
2022-11-13 16:40:23 +0100

    ADD - outsource position generator to an additional class

---
+++ AgiliTEAM/solid_version/core/misc/pos_generator.py
@@ -0,0 +1,34 @@
+from typing import List, Tuple
+import numpy as np
+
+from bosch_ASDIIE.AgiliTEAM.solid_version.core.misc.map import MapSize, Coordinates
+
+
+class PositionGenerator:
+
+    def __init__(self,
+                 map_size: MapSize,
+                 known_pos: List[Coordinates]):
+        """
+
+        :param map_size:
+        :param known_pos:
+        """
+        self.map_size = map_size
+        self.known_pos = known_pos
+
+    def generate_pos(self, num_of_pos: int) -> Tuple[List[Coordinates], List[Coordinates]]:
+        """
+
+        :param num_of_pos:
+        :return:
+        """
+        pos_list = []
+        for pos_num in range(num_of_pos):
+            pos = Coordinates(np.random.randint(self.map_size[0]), np.random.randint(self.map_size[1]))
+            while pos in self.known_pos:
+                pos = Coordinates(np.random.randint(self.map_size[0]), np.random.randint(self.map_size[1]))
+            pos_list.append(pos)
+            self.known_pos.append(pos)
+
+        return pos_list, self.known_pos
