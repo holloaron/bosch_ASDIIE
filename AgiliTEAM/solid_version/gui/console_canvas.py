359ae5838ec71a1bbfbac25f73be52956ad70ad9
BPelenczei <81440132+BPelenczei@users.noreply.github.com>
2022-11-13 18:24:33 +0100

    CHANGE dict in console_canvas.py to enum (Object markers)

--- AgiliTEAM/solid_version/gui/console_canvas.py
+++ AgiliTEAM/solid_version/gui/console_canvas.py
@@ -4,17 +4,16 @@ from typing import List
 from core.misc.map import Coordinates, MapSize
 from core.interface.canvas import Canvas
 from core.display.screen import Screen
+from core.display.object_markers import ObjectMarkers
 
 
 class ConsoleCanvas(Canvas):
-
     OBJECT_MARKERS = {
-        "pacman": '@',
-        "ghosts": 'x',
-        "pellets": 'o',
-        "walls": '#'
+        "walls": ObjectMarkers.WALLS,
+        "pacman": ObjectMarkers.PACMAN,
+        "ghosts": ObjectMarkers.GHOSTS,
+        "pellets": ObjectMarkers.PELLETS,
     }
-
     def __init__(self,
                  map_size: MapSize,
                  curses_screen: Screen):
