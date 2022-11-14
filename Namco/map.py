4e019ac7fdd7ae03259e21cc75099ae5945418be
farkas-peter <peterfarkas@edu.bme.hu>
2022-11-14 18:51:40 +0100

    Random map generation is added, map loading is removed

--- Namco/map.py
+++ Namco/map.py
@@ -2,19 +2,66 @@ import numpy as np
 
 
 class Map:
-    def __init__(self, map_source: str = "map.txt"):
+    def __init__(self, map_size: int = 10, agent_slot: str = '0', restricted_slot: str = '#', award_slot: str = '-',
+                 terminating_slot: str = 'X', empty_slot: str = ' '):
         """
-        :param map_source: name of the map file (str)
-        Map class: Initializing the map with walls and dots
+        :param map_size: size of the map (int)
+        Map class: Initializing the map with restricted areas and award slots
         """
-        # Loading map from the .txt file
-        self.map = np.loadtxt(map_source, dtype=str, delimiter=',')
-        # Filling up the map with dots
-        self.map[self.map == ' '] = 'x'
+        # Markers for different objects
+        self.agent_slot = agent_slot
+        self.restricted_slot = restricted_slot
+        self.award_slot = award_slot
+        self.terminating_slot = terminating_slot
+        self.empty_slot = empty_slot
+
+        # Generating the map
+        self.map_size = map_size
+        self.map = np.ndarray(shape=(self.map_size, self.map_size), dtype='<U1')
+        self._generate_map()
+
+    def _create_map_borders(self):
+        """
+        Defines the borders of the map as restricted area
+        :return: -
+        """
+        self.map[0, :] = self.restricted_slot
+        self.map[-1, :] = self.restricted_slot
+        self.map[:, 0] = self.restricted_slot
+        self.map[:, -1] = self.restricted_slot
+
+    def _create_restricted_areas(self):
+        """
+        Creating restricted areas on the map
+        :return: -
+        """
+        # Restricted areas should not be near the top or bottom wall to avoid unreachable areas
+        for row in range(2, self.map_size - 2):
+            # Restricted area cannot start near vertical map borders to avoid unreachable areas
+            area_start = np.random.randint(2, self.map_size - 2)
+            area_length = np.random.randint(self.map_size - 1 - area_start)
+            # Placing restricted area on the map
+            self.map[row, area_start:(area_start+area_length)] = self.restricted_slot
+
+    def _create_award_slots(self):
+        """
+        Filling the empty areas with award slots
+        :return: -
+        """
+        self.map[self.map != self.restricted_slot] = self.award_slot
+
+    def _generate_map(self):
+        """
+        Places every object on the map
+        :return:
+        """
+        self._create_map_borders()
+        self._create_restricted_areas()
+        self._create_award_slots()
 
     def update_map(self, agent_pos: [int, int], done: bool) -> np.ndarray:
         """
-        Updates the map based on PacMan's position
+        Updates the map based on the agent's position
         :param agent_pos: x and y coordinate of the agent (int, int)
         :param done: Game over flag (bool)
         :return: Updated map (np.ndarray)
@@ -22,32 +69,34 @@ class Map:
         x = agent_pos[0]
         y = agent_pos[1]
 
-        # PacMan's previous position must be emptied
-        self.map[self.map == '0'] = ' '
-        # PacMan should be placed into its current position
-        self.map[x][y] = '0'
-        # Marking place of death on map
+        # Agent's previous position must be emptied
+        self.map[self.map == self.agent_slot] = self.empty_slot
+
+        # The agent should be placed into its current position
+        self.map[x][y] = self.agent_slot
+
+        # Marking place of agent's death on map
         if done:
-            self.map[self.map == '0'] = 'X'
+            self.map[self.map == self.agent_slot] = self.terminating_slot
 
         return self.map
 
-    def check_collision(self, agent_pos: [int, int]) -> [bool, bool]:
+    def check_interaction(self, agent_pos: [int, int]) -> [bool, bool]:
         """
-        Checks whether Pacman interacted with any objects on the map
-        :return: Wall collision flag (bool), dot collision flag (bool)
+        Checks whether the agent interacted with any objects on the map
+        :return: Restricted slot flag (bool), award slot flag (bool)
         """
-        dot_collision = False
-        wall_collision = False
+        award_interaction = False
+        restricted_interaction = False
 
         x = agent_pos[0]
         y = agent_pos[1]
 
-        # Checking whether PacMan stepped on a wall
-        if self.map[x][y] == '-' or self.map[x][y] == '|':
-            wall_collision = True
-        # Checking whether PacMan collected an eatable dot
-        elif self.map[x][y] == 'x':
-            dot_collision = True
+        # Checking whether the agent interacted with an award slot
+        if self.map[x][y] == self.award_slot:
+            award_interaction = True
+        # Checking whether the agent interacted with a restricted slot
+        elif self.map[x][y] == self.restricted_slot:
+            restricted_interaction = True
 
-        return dot_collision, wall_collision
+        return award_interaction, restricted_interaction
