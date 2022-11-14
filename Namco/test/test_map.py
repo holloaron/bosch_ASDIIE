99392f7a22fd89d78143693148c5c9d489bbf7fe
Kurtiadam <adamkurti97@gmail.com>
2022-11-14 23:46:05 +0100

    ADD: unit tests for _create_restricted_areas method

--- Namco/test/test_map.py
+++ Namco/test/test_map.py
@@ -36,6 +36,17 @@ class MyTestCase(unittest.TestCase):
         "When the map is generated all bordering tiles should be wall tiles" \
         "independent of the map size"
 
+    # Unit testing for the _create_restricted_areas method
+    def test_create_restricted_areas_whenMapIsGenerated_RestrictedAreasCannotBeNextToBorder(self):
+        env = Map(map_size=10, agent_slot='0', restricted_slot='#',
+                  award_slot='×', terminating_slot='X', empty_slot=' ')
+        env._generate_map()
+        self.assertFalse((env.map[1, 1:-2] == env.restricted_slot).all())
+        self.assertFalse((env.map[-2, 1:-2] == env.restricted_slot).all())
+        self.assertFalse((env.map[1:-2, 1] == env.restricted_slot).all())
+        self.assertFalse((env.map[1:-2, -2] == env.restricted_slot).all())
+        "When the inner walls are generated, no wall tile should be on the" \
+        "neighboring position with the outer border"
 
 if __name__ == '__main__':
     unittest.main()
