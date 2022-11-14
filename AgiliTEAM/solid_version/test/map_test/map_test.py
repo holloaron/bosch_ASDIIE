ed5ad4527f375929986cde368d0cdc9deb9fd6b9
BPelenczei <81440132+BPelenczei@users.noreply.github.com>
2022-11-14 09:25:28 +0100

    FIX PEP8 format

--- AgiliTEAM/solid_version/test/map_test/map_test.py
+++ AgiliTEAM/solid_version/test/map_test/map_test.py
@@ -12,25 +12,23 @@ from core.misc.map import Coordinates, MapSize
 class MapTest(unittest.TestCase):
 
     def test_MapMaking_NotEmpty(self):
-        map_size = MapSize(5,5)
-        internal_walls = [Coordinates(3,3)]
+        map_size = MapSize(5, 5)
+        internal_walls = [Coordinates(3, 3)]
         walls = Walls(map_size=map_size, internal_walls=internal_walls)
-        self.assertTrue(walls.pos.count(Coordinates(3,3)) == 1,"No inner wall detected on the pitch")
+        self.assertTrue(walls.pos.count(Coordinates(3, 3)) == 1, "No inner wall detected on the pitch")
 
     def test_MapMaking_Borders(self):
         map_size = MapSize(3, 3)
         walls = Walls(map_size=map_size)
 
         for row in range(map_size.row_num):
-            self.assertEqual(walls.pos.count(Coordinates(row, 0)),1,"The pitch generation is wrong. There is a leak in the pitch borders")
+            self.assertEqual(walls.pos.count(Coordinates(row, 0)), 1, "The pitch generation is wrong. There is a leak"
+                                                                      "in the pitch borders")
             self.assertEqual(walls.pos.count(Coordinates(row, map_size.col_num - 1)), 1,
                              "The pitch generation is wrong. There is a leak in the pitch borders")
 
-
     def test_MapMaking_Empty(self):
-        map_size = MapSize(5,5)
-        internal_walls = [Coordinates(3,3)]
+        map_size = MapSize(5, 5)
+        internal_walls = [Coordinates(3, 3)]
         walls = Walls(map_size=map_size, internal_walls=internal_walls)
-        self.assertTrue(walls.pos.count(Coordinates(2,2)) == 0,"Inner wall detected")
-
-
+        self.assertTrue(walls.pos.count(Coordinates(2, 2)) == 0, "Inner wall detected")
